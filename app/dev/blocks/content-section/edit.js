99c02292db78608d16b6d02d3753340ed5ceee48
Alex Ferrao <alexsr20det@gmail.com>
2024-07-19 12:38:51 -0400

    Dev

--- app/dev/blocks/content-section/edit.js
+++
@@ -1,5 +0,0 @@
-import { useBlockProps } from "@wordpress/block-editor"
-import { registerBlockType } from "@wordpress/blocks"
-import metadata from "./block.json"
-
-registerBlockType(metadata.name)
---
+++ app/dev/blocks/content-section/js/edit.js
@@ -0,0 +1,21 @@
+import { useBlockProps } from "@wordpress/block-editor"
+import { registerBlockType } from "@wordpress/blocks"
+import ServerSideRender from '@wordpress/server-side-render';
+import metadata from "../block.json"
+
+registerBlockType(metadata.name,{
+    edit: () => {
+        return (
+            <>
+                <ServerSideRender
+                    block={metadata.name}
+                />
+            </>
+        );
+    },
+    save() {
+        return null; // Nothing to save here..
+    }
+})
+
+

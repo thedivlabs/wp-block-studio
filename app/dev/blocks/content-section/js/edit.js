54b4c2f99a919d1b0d409279c201b7171e1bb368
Alex Ferrao <alexsr20det@gmail.com>
2024-07-20 10:34:52 -0400

    Dev

--- app/dev/blocks/content-section/js/edit.js
+++ app/dev/blocks/content-section/js/edit.js
@@ -1,23 +1,66 @@
-import {useBlockProps, BlockEdit,InspectorControls,PanelColorSettings,RichText} from "@wordpress/block-editor"
+
+
+import {useBlockProps,InspectorControls,RichText} from "@wordpress/block-editor"
+import {SelectControl,ToggleControl,PanelBody} from "@wordpress/components"
 import {registerBlockType} from "@wordpress/blocks"
 import metadata from "../block.json"
 
 registerBlockType(metadata.name, {
     attributes: {
         content: {
-            type: 'string',
-            source: 'html',
-            selector: 'div',
+            type: 'string'
+        },
+        toggleField: {
+            type: 'boolean'
+        },
+        selectField: {
+            type: 'string'
         }
     },
-    edit: ({attributes, setAttributes, isSelected, blockProps}) => {
+    edit: ({attributes, setAttributes}) => {
+        const blockProps = useBlockProps();
+        const {
+            content,
+            toggleField,
+            selectField,
+        } = attributes;
 
         function onChangeContent( newContent ) {
             setAttributes( { content: newContent } );
         }
 
+        function onChangeToggleField( newValue ) {
+            setAttributes( { toggleField: newValue } );
+        }
+
+        function onChangeSelectField( newValue ) {
+            setAttributes( { selectField: newValue } );
+        }
+
         return (
             <>
+                <InspectorControls>
+                    <PanelBody title={ 'Settings Test' }>
+
+                        <ToggleControl
+                            label="Toggle Field"
+                            checked={ toggleField }
+                            onChange={ onChangeToggleField }
+                        />
+
+                        <SelectControl
+                            label="Select Control"
+                            value={ selectField }
+                            options={ [
+                                { value: 'a', label: 'Option A' },
+                                { value: 'b', label: 'Option B' },
+                                { value: 'c', label: 'Option C' },
+                            ] }
+                            onChange={ onChangeSelectField }
+                        />
+                    </PanelBody>
+                </InspectorControls>
+
                 <RichText
                     { ...blockProps }
                     key="editable"
@@ -28,14 +71,17 @@ registerBlockType(metadata.name, {
             </>
         )
     },
-    save: ({attributes}) => {
-        const blockProps = useBlockProps.save({
-            className: 'wpbs-content-section'
-        });
-        return <div {...blockProps}>
-            <RichText.Content value={attributes.content} tagName="p"/>
-            aaaaaaa
-        </div>;
+    save: ({attributes}) =>{
+        const {
+            content,
+        } = attributes;
+        const blockProps = useBlockProps.save();
+
+        return (
+            <section { ...blockProps } className={'wpbs-content-section'}>
+                <RichText.Content value={ content } tagName={'div'} className={'container wpbs-container'} />
+            </section>
+        );
     }
 })
 

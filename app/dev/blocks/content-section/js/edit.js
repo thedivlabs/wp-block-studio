0c66da4197bfc3beb255da054f51042d31d81355
Alex Ferrao <alexsr20det@gmail.com>
2024-07-20 10:04:40 -0400

    Dev

--- app/dev/blocks/content-section/js/edit.js
+++ app/dev/blocks/content-section/js/edit.js
@@ -1,22 +1,41 @@
-import {useBlockProps} from "@wordpress/block-editor"
+import {useBlockProps, BlockEdit,InspectorControls,PanelColorSettings,RichText} from "@wordpress/block-editor"
 import {registerBlockType} from "@wordpress/blocks"
 import metadata from "../block.json"
 
 registerBlockType(metadata.name, {
-    edit: ( { attributes, setAttributes, isSelected } ) => {
-        const blockProps = useBlockProps();
+    attributes: {
+        content: {
+            type: 'string',
+            source: 'html',
+            selector: 'div',
+        }
+    },
+    edit: ({attributes, setAttributes, isSelected, blockProps}) => {
 
-        const { content, mySetting } = attributes;
+        function onChangeContent( newContent ) {
+            setAttributes( { content: newContent } );
+        }
 
         return (
-            <div { ...blockProps }>
-                { content }
-            </div>
-        );
+            <>
+                <RichText
+                    { ...blockProps }
+                    key="editable"
+                    tagName="p"
+                    onChange={ onChangeContent }
+                    value={ content }
+                />
+            </>
+        )
     },
-    save: ()=> {
-        const blockProps = useBlockProps.save();
-        return <div { ...blockProps }> Your block. </div>;
+    save: ({attributes}) => {
+        const blockProps = useBlockProps.save({
+            className: 'wpbs-content-section'
+        });
+        return <div {...blockProps}>
+            <RichText.Content value={attributes.content} tagName="p"/>
+            aaaaaaa
+        </div>;
     }
 })
 

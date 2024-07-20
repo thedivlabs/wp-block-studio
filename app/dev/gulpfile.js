4a45ddeba1dad40d6b703a2b1bb2f4c1621e2cf4
Alex Ferrao <alexsr20det@gmail.com>
2024-07-20 16:11:33 -0400

    Dev

--- app/dev/gulpfile.js
+++ app/dev/gulpfile.js
@@ -5,7 +5,9 @@ const postcss = require('gulp-postcss');
 const tailwindcss = require('tailwindcss');
 const minify = require('gulp-minify');
 const webpack = require('webpack-stream');
-const cleanCSS = require('gulp-clean-css');//To Minify CSS files
+const cleanCSS = require('gulp-clean-css');
+const node_path = require("path");
+//To Minify CSS files
 
 
 const scss_include_paths = [ './includes'];
@@ -14,7 +16,7 @@ const scss_include_paths = [ './includes'];
 
 gulp.task('theme_css', function () {
     return gulp.src([
-        "./scss/theme.scss"
+        './scss/theme.scss',
     ])
         .pipe(sass({
             includePaths: './scss/includes/',
@@ -27,19 +29,44 @@ gulp.task('theme_css', function () {
             extname: ".min.css",
         }))
         .pipe(cleanCSS())
-        .pipe(gulp.dest('../public/wp-content/themes/wp-block-studio/dist/css/'))
+        .pipe(gulp.dest('../public/wp-content/themes/wp-block-studio/dist/'))
+});
+
+gulp.task('blocks_css', function () {
+    return gulp.src([
+        './blocks/**/scss/block.scss'
+    ], {base: "."})
+        .pipe(sass({
+            includePaths: scss_include_paths,
+            //outputStyle: 'compressed'
+        },false).on('error', sass.logError))
+        .pipe(postcss([
+            tailwindcss('./tailwind.config.js')
+        ], false))
+        .pipe(rename(function (path) {
+            return {
+                basename: path.basename,
+                dirname: node_path.basename(node_path.resolve(path.dirname + '/', '..') + '/'),
+                extname: ".min.css",
+            };
+
+        }))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('../public/wp-content/themes/wp-block-studio/dist/blocks/'))
 });
 
 /* RUN TASKS */
 
 gulp.task('build', gulp.series([
-    'theme_css',
+    'theme_css','blocks_css',
 ]));
 
 gulp.task('default', function () {
     gulp.watch(
         [
-            './**/*.{js,scss}'
+            './*.{js,scss}',
+            './**/*.{js,scss}',
+            './**/**/*.{js,scss}',
         ],
         {
             ignoreInitial: false

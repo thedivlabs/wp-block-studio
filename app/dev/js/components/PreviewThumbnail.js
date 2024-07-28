7bfe75f09456c5dcc6b9e55d8d629c10ef42f964
Alex Ferrao <alexsr20det@gmail.com>
2024-07-28 10:47:50 -0400

    Dev

--- app/dev/js/components/PreviewThumbnail.js
+++ app/dev/js/components/PreviewThumbnail.js
@@ -1,19 +1,56 @@
+import {Button} from '@wordpress/components'
+
 function PreviewThumbnail({image = {}, callback}) {
 
     return (
-        <img src={image.url || '#'}
+        <div style={{
+            width: '100%',
+            display: 'flex',
+            backgroundColor: '#efefef',
+            borderRadius: '4px',
+            padding: '5px',
+            position: 'relative',
+            cursor: 'pointer'
+        }}
              onClick={() => {
                  if (callback) {
                      callback(image);
                  }
              }}
-             alt={''}
-             style={{
-                 cursor: 'pointer',
-                 width: '60px',
-                 objectFit: 'cover',
-                 height: '60px'
-             }}/>
+        >
+            <img src={image.url || '#'}
+                 alt={''}
+                 style={{
+                     cursor: 'pointer',
+                     width: '100%',
+                     objectFit: 'cover',
+                     borderRadius: '4px',
+                     aspectRatio: '16/9',
+                     pointerEvents: 'none'
+                 }}/>
+            <Button
+                icon={'no-alt'}
+                style={{
+                    position: 'absolute',
+                    top: '10px',
+                    right: '10px',
+                    zIndex: '10',
+                    width: '20px',
+                    height: '20px',
+                    lineHeight: '20px',
+                    borderRadius: '20px',
+                    backgroundColor: 'white',
+                    color: 'black',
+                    fontSize: '14px',
+                    overflow: 'hidden',
+                    padding: '0',
+                    margin: '0',
+                    textAlign: 'center',
+                    justifyContent: 'center',
+                    minWidth: '0',
+                    pointerEvents: 'none'
+                }}></Button>
+        </div>
     )
 }
 

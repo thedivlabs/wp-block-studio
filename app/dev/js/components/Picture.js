f8d7af8e9ee3929103a5bd86304adc8ca5bd2714
Alex Ferrao <alexsr20det@gmail.com>
2024-07-28 21:45:40 -0400

    Dev

--- app/dev/js/components/Picture.js
+++ app/dev/js/components/Picture.js
@@ -3,7 +3,11 @@ function Picture({mobile = {}, large = {}, settings = {}}) {
     const {medium: mobileMedium = {}, large: mobileLarge = {}} = mobile.sizes || {};
     const {medium: largeMedium = {}, large: largeLarge = {}} = large.sizes || {};
 
-    if (!mobileMedium && !mobileLarge && !largeMedium && !largeLarge) {
+    const urlLarge = largeLarge.url || mobileLarge.url || false;
+    const urlMedium = largeMedium.url || mobileMedium.url || false;
+    const urlMobile = mobileMedium.url || largeLarge.url || false;
+
+    if (!urlLarge && !urlMobile) {
         return false;
     }
 
@@ -13,14 +17,10 @@ function Picture({mobile = {}, large = {}, settings = {}}) {
     ].filter(x => x).join(' ');
 
 
-    const urlLarge = largeLarge.url || mobileLarge.url || '#';
-    const urlMedium = largeMedium.url || mobileMedium.url || '#';
-    const urlMobile = mobileMedium.url || largeLarge.url || '#';
-
     return <picture className={className}>
-        <source src={urlLarge} media={'@media screen and (min-width: 1140px)'}/>
-        <source src={urlMedium} media={'@media screen and (min-width: 960px)'}/>
-        <source src={urlMobile} media={'@media screen and (min-width: 0px)'}/>
+        <source src={urlLarge || '#'} media={'@media screen and (min-width: 1140px)'}/>
+        <source src={urlMedium || '#'} media={'@media screen and (min-width: 960px)'}/>
+        <source src={urlMobile || '#'} media={'@media screen and (min-width: 0px)'}/>
         <img src={urlLarge} alt={large.alt || mobile.alt || ''} aria-hidden={'true'}
              loading={settings.eager ? 'eager' : 'lazy'}/>
     </picture>;

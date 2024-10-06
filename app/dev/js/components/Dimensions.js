7e81745c0309463ce97edae84b4ce63f3e7d05c4
Alex Ferrao <alexsr20det@gmail.com>
2024-10-06 07:57:20 -0400

    Dev

--- app/dev/js/components/Dimensions.js
+++ app/dev/js/components/Dimensions.js
@@ -3,10 +3,12 @@ import {TabPanel} from '@wordpress/components'
 import {
     __experimentalGrid as Grid,
     PanelBody,
-    SelectControl,
-    ToggleControl,
-    __experimentalNumberControl as NumberControl,
+    BoxControl,
 } from "@wordpress/components";
+import {
+    HeightControl
+} from '@wordpress/block-editor';
+
 
 export function Dimensions({attr = {}, update}) {
 
@@ -40,21 +42,86 @@ export function Dimensions({attr = {}, update}) {
 
     function Panels(tab) {
         return <Grid columns={1} columnGap={20} rowGap={30}>
-            <SelectControl
-                label={'Display'}
-                value={settings[tab.name].display}
-                options={options.display}
+            <HeightControl
+                label={'Height'}
                 onChange={(value) => {
-                    updateSettings(tab.name, 'display', value);
+                    updateSettings(tab.name, 'height', value);
                 }}
-                __nextHasNoMarginBottom
+                value={settings[tab.name].height}
             />
-
+            <HeightControl
+                label={'Width'}
+                onChange={(value) => {
+                    updateSettings(tab.name, 'width', value);
+                }}
+                value={settings[tab.name].width}
+            />
+            <HeightControl
+                label={'Max-Width'}
+                onChange={(value) => {
+                    updateSettings(tab.name, 'maxWidth', value);
+                }}
+                value={settings[tab.name].maxWidth}
+            />
+            <HeightControl
+                label={'Basis'}
+                onChange={(value) => {
+                    updateSettings(tab.name, 'basis', value);
+                }}
+                value={settings[tab.name].basis}
+            />
         </Grid>
     }
 
     function PanelsMobile(tab) {
         return <Grid columns={1} columnGap={20} rowGap={30}>
+            <BoxControl
+                label="Padding"
+                onChange={(value) => {
+                    updateSettings(tab.name, 'padding', value);
+                }}
+            />
+            <BoxControl
+                label="Margin"
+                onChange={(value) => {
+                    updateSettings(tab.name, 'margin', value);
+                }}
+            />
+            <HeightControl
+                label={'Gap'}
+                onChange={(value) => {
+                    updateSettings(tab.name, 'gap', value);
+                }}
+                value={settings[tab.name].gap}
+            />
+            <HeightControl
+                label={'Height'}
+                onChange={(value) => {
+                    updateSettings(tab.name, 'height', value);
+                }}
+                value={settings[tab.name].height}
+            />
+            <HeightControl
+                label={'Width'}
+                onChange={(value) => {
+                    updateSettings(tab.name, 'width', value);
+                }}
+                value={settings[tab.name].width}
+            />
+            <HeightControl
+                label={'Max-Width'}
+                onChange={(value) => {
+                    updateSettings(tab.name, 'maxWidth', value);
+                }}
+                value={settings[tab.name].maxWidth}
+            />
+            <HeightControl
+                label={'Basis'}
+                onChange={(value) => {
+                    updateSettings(tab.name, 'basis', value);
+                }}
+                value={settings[tab.name].basis}
+            />
         </Grid>
     }
 

2c528dccaf2e1d6683cc2dfe7d5c1f21cd38da51
thedivlabs <121818117+thedivlabs@users.noreply.github.com>
2025-03-24 15:21:48 -0400

    Dev

--- app/dev/blocks/video/js/view.js
+++ app/dev/blocks/video/js/view.js
@@ -1,18 +1,34 @@
-import {store, getElement,getContext} from '@wordpress/interactivity';
+import {store, getElement, getContext} from '@wordpress/interactivity';
 
 const {state} = store('wpbs', {
     callbacks: {
         videoModal: () => {
             const {ref: element} = getElement();
-            const {url,title} = getContext();
+            const {url, title} = getContext();
+            const embed_base = 'https://www.youtube.com/embed/';
+            const vid = (new URL(url)).pathname;
 
-            console.log(url);
-            console.log(title);
+            const player = jQuery('<iframe />', {
+                src: embed_base + vid + '?autoplay=1&enablejsapi=1&rel=0',
+                allow: 'autoplay;',
+                allowFullScreen: true,
+                title: 'YouTube video player',
+                frameBorder: 0,
+                width: '100%',
+                height: '100%',
+            }).css({opacity: 0, transition: 'opacity .5s'}).on('load', function () {
+                jQuery(this).css({opacity: 1})
+                WPBS.loader.toggle({
+                    remove: true
+                });
+            });
+
+            const component = jQuery('<div />', {
+                class: 'divlabs-video-player'
+            }).append(player);
 
             WPBS.modals.toggle_modal(false, {
-                template: jQuery('<div/>',{
-                    class:'test-modal'
-                }).text('QQQ').get(0)
+                template: component.get(0)
             });
 
         },

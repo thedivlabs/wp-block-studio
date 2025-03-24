d225575439b80ee068e0af0c6ec10a379a5fcec5
Alex Ferrao <alexsr20det@gmail.com>
2025-03-24 10:26:03 -0400

    Dev

--- app/dev/js/inc/helper.js
+++ app/dev/js/inc/helper.js
@@ -19,5 +19,16 @@ export function parseProp(prop) {
 }
 
 export function breakpoint(prop) {
-    
+
 }
+
+export const imageButtonStyle = {
+    border: '1px dashed lightgray',
+    width: '100%',
+    height: 'auto',
+    aspectRatio: '16/9',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+};

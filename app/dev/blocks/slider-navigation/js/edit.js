fd42790f4e4a3029f4e8f8c199c5295bb9942e01
thedivlabs <121818117+thedivlabs@users.noreply.github.com>
2025-03-28 10:42:29 -0400

    Dev

---
+++ app/dev/blocks/slider-navigation/js/edit.js
@@ -0,0 +1,52 @@
+import {
+    useBlockProps,
+    BlockEdit,
+    useInnerBlocksProps
+} from "@wordpress/block-editor"
+import {registerBlockType} from "@wordpress/blocks"
+import metadata from "../block.json"
+import React from "react";
+
+function blockClasses(attributes = {}) {
+    return [
+        'wpbs-slider-navigation',
+    ].filter(x => x).join(' ');
+}
+
+const BlockContent = <>NAVIGATION</>;
+
+registerBlockType(metadata.name, {
+    apiVersion: 3,
+    attributes: {
+        ...metadata.attributes,
+    },
+    edit: ({attributes, setAttributes, clientId}) => {
+
+        const blockProps = useBlockProps({
+            className: blockClasses(attributes),
+        });
+
+        const innerBlocksProps = useInnerBlocksProps(blockProps, {
+            template: [
+                ['wpbs/slide', {content: 'Content Slide'}],
+            ]
+        });
+
+        return <>
+            <BlockEdit key="edit" {...blockProps} />
+            <BlockContent/>
+        </>;
+    },
+    save: (props) => {
+
+        const blockProps = useBlockProps.save({
+            className: blockClasses(props.attributes),
+        });
+
+        return (
+            <BlockContent/>
+        );
+    }
+})
+
+

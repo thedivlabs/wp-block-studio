7c1b7f041db65c78924cbdfb62e38a2f700c5798
thedivlabs <121818117+thedivlabs@users.noreply.github.com>
2025-03-30 10:41:17 -0400

    Dev

--- app/dev/blocks/slide/js/edit.js
+++ app/dev/blocks/slide/js/edit.js
@@ -3,7 +3,7 @@ import '../scss/block.scss';
 import {
     useBlockProps,
     BlockEdit,
-    useInnerBlocksProps, InspectorControls
+    useInnerBlocksProps, InspectorControls, MediaUploadCheck, MediaUpload
 } from "@wordpress/block-editor"
 import {registerBlockType} from "@wordpress/blocks"
 import metadata from "../block.json"
@@ -12,6 +12,10 @@ import {Background, BackgroundSettings, BackgroundAttributes} from "Components/B
 
 import React, {useEffect, useState} from "react";
 import {useInstanceId} from '@wordpress/compose';
+import {__experimentalGrid as Grid, BaseControl, Button, ToggleControl} from "@wordpress/components";
+import PreviewThumbnail from "Components/PreviewThumbnail.js";
+import {imageButtonStyle} from "Includes/helper.js";
+import Resolution from "Components/Resolution.js";
 
 function blockClasses(attributes = {}) {
     return [
@@ -22,8 +26,17 @@ function blockClasses(attributes = {}) {
 }
 
 const blockAttributes = {
-    'wpbs-title': {
+    'wpbs-mobileImage': {
         type: 'string'
+    },
+    'wpbs-largeImage': {
+        type: 'string'
+    },
+    'wpbs-resolution': {
+        type: 'string'
+    },
+    'wpbs-eager': {
+        type: 'boolean'
     },
 }
 
@@ -37,7 +50,10 @@ registerBlockType(metadata.name, {
     },
     edit: ({attributes, setAttributes, clientId}) => {
 
-        const [title, setTitle] = useState(attributes['wpbs-title']);
+        const [mobileImage, setMobileImage] = useState(attributes['wpbs-mobileImage']);
+        const [largeImage, setLargeImage] = useState(attributes['wpbs-largeImage']);
+        const [resolution, setResolution] = useState(attributes['wpbs-resolution']);
+        const [eager, setEager] = useState(attributes['wpbs-eager']);
 
         const uniqueId = useInstanceId(registerBlockType, 'wpbs-slide');
 
@@ -53,13 +69,135 @@ registerBlockType(metadata.name, {
             className: 'wpbs-slide__container w-full h-full container relative z-20',
         });
 
+        const Controls = !blockProps.className.split(' ').includes('is-style-image') ? <InspectorControls group="styles">
+            <BackgroundSettings attributes={attributes || {}}
+                                pushSettings={setAttributes}></BackgroundSettings>
+        </InspectorControls> : <InspectorControls group="styles">
+            <Grid columns={1} columnGap={15} rowGap={20} >
+
+                <Grid columns={2} columnGap={15} rowGap={20} >
+                    <BaseControl label={'Mobile Image'} __nextHasNoMarginBottom={true}>
+                        <MediaUploadCheck>
+                            <MediaUpload
+                                title={'Mobile Image'}
+                                onSelect={(value) => {
+                                    setMobileImage({
+                                        type: value.type,
+                                        id: value.id,
+                                        url: value.url,
+                                        alt: value.alt,
+                                        sizes: value.sizes,
+                                    });
+                                    setAttributes({
+                                        ['wpbs-mobileImage']: {
+                                            type: value.type,
+                                            id: value.id,
+                                            url: value.url,
+                                            alt: value.alt,
+                                            sizes: value.sizes,
+                                        }
+                                    });
+                                }}
+                                allowedTypes={['image']}
+                                value={mobileImage}
+                                render={({open}) => {
+                                    return <PreviewThumbnail
+                                        image={mobileImage || {}}
+                                        callback={() => {
+                                            setMobileImage(undefined);
+                                            setAttributes({['wpbs-mobileImage']: undefined});
+                                        }}
+                                        onClick={open}
+                                    />;
+                                }}
+                            />
+                        </MediaUploadCheck>
+                    </BaseControl>
+                    <BaseControl label={'Large Image'} __nextHasNoMarginBottom={true}>
+                        <MediaUploadCheck>
+                            <MediaUpload
+                                title={'Large Image'}
+                                onSelect={(value) => {
+                                    setLargeImage({
+                                        type: value.type,
+                                        id: value.id,
+                                        url: value.url,
+                                        alt: value.alt,
+                                        sizes: value.sizes,
+                                    });
+                                    setAttributes({
+                                        ['wpbs-largeImage']: {
+                                            type: value.type,
+                                            id: value.id,
+                                            url: value.url,
+                                            alt: value.alt,
+                                            sizes: value.sizes,
+                                        }
+                                    });
+                                }}
+                                allowedTypes={['image']}
+                                value={largeImage}
+                                render={({open}) => {
+                                    if (largeImage) {
+                                        return <PreviewThumbnail
+                                            image={largeImage || {}}
+                                            callback={() => {
+                                                setLargeImage(undefined);
+                                                setAttributes({['wpbs-largeImage']: undefined});
+                                            }}
+                                            onClick={open}
+                                        />;
+                                    } else {
+                                        return <Button onClick={open} style={imageButtonStyle}>Choose
+                                            Image</Button>
+                                    }
+                                }}
+                            />
+                        </MediaUploadCheck>
+                    </BaseControl>
+
+                    <Resolution defaultValue={attributes['wpbs-resolution']} callback={(newValue) => {
+                        setAttributes({['wpbs-resolution']: newValue});
+                        setResolution(newValue)
+                    }}/>
+                </Grid>
+
+                <Grid columns={2} columnGap={15} rowGap={20}
+                      style={{padding: '1rem 0'}}>
+                    <ToggleControl
+                        label="Eager"
+                        checked={eager}
+                        onChange={(value) => {
+                            setEager(value);
+                            setAttributes({['wpbs-eager']: value});
+
+                            if (value) {
+                                setAttributes({
+                                    preload: [
+                                        {
+                                            mobile: !!attributes['wpbs-mobileImage'] ? attributes['wpbs-mobileImage'].id : null,
+                                            large: !!attributes['wpbs-largeImage'] ? attributes['wpbs-largeImage'].id : null,
+                                            size: attributes['wpbs-resolution'] || null
+                                        }
+                                    ]
+                                });
+
+                            }
+                        }}
+                        className={'flex items-center'}
+                        __nextHasNoMarginBottom
+                    />
+
+
+
+                </Grid>
+            </Grid>
+        </InspectorControls>;
+
         return <>
             <BlockEdit key="edit" {...blockProps} />
 
-            <InspectorControls group="styles">
-                <BackgroundSettings attributes={attributes || {}}
-                                    pushSettings={setAttributes}></BackgroundSettings>
-            </InspectorControls>
+
             <Layout blockProps={blockProps} attributes={attributes} setAttributes={setAttributes}
                     clientId={clientId}></Layout>
 

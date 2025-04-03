29f582011f1d118a3215a7aa97bf038a6c021adf
Alex Ferrao <alexsr20det@gmail.com>
2025-04-03 11:31:35 -0400

    Dev

--- app/dev/js/components/LayoutStyle.js
+++ app/dev/js/components/LayoutStyle.js
@@ -16,6 +16,7 @@ export const LayoutStyle = ({attributes, clientId}) => {
             },
         })
             .then((data) => {
+                console.log(data);
                 setCss(data);
             })
     }, [attributes]);

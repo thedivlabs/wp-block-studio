23b0ed4c7f82a4e0fa141f64c61f0e94aa85e8c3
thedivlabs <121818117+thedivlabs@users.noreply.github.com>
2025-04-20 10:54:11 -0400

    Dev

--- app/dev/blocks/cta-button/render.php
+++ app/dev/blocks/cta-button/render.php
@@ -3,9 +3,7 @@
 WPBS_Grid::render_style( $attributes ?? false, $block ?? false, false );
 
 $replacements = [
-	'%%URL%%'   => esc_url( $attributes['url'] ?? '#' ),
-	'%%TARGET%%' => esc_attr( $attributes['target'] ?? '_self' ),
-	'%%TITLE%%' => esc_html( $attributes['title'] ?? 'Click' ),
+	'%%URL%%'   => !empty($attributes['wpbs-loop']) ? get_the_permalink() : esc_url( $attributes['wpbs-link']['url'] ?? '#' ),
 ];
 
 echo strtr( $content ?? '', $replacements );

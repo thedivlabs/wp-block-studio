e00ed175f09860c51857ef0f522e46cbe1c83f4e
thedivlabs <121818117+thedivlabs@users.noreply.github.com>
2025-04-19 20:00:54 -0400

    Dev

--- app/dev/blocks/cta-button/render.php
+++ app/dev/blocks/cta-button/render.php
@@ -1,8 +1,12 @@
 <?php
 
-if ( ! empty( $attibutes['wpbs-loop'] ) ) {
+
+
+
+if ( ! empty( $attributes['wpbs-loop'] ) ) {
 	global $post;
-	$content = str_replace( 'href="#"', get_the_permalink( $post ), $content ?? '' );
+	$content = str_replace( 'href="#"', 'href="' . get_the_permalink( $post ) . '"', $content ?? '' );
+
 }
 
 echo $content ?? false;

750ce066b93ff123887e00b5ace6b39fb6044529
thedivlabs <121818117+thedivlabs@users.noreply.github.com>
2025-05-26 13:56:19 -0400

    Dev

--- app/public/wp-content/themes/wp-block-studio/core/class-wpbs.php
+++ app/public/wp-content/themes/wp-block-studio/core/class-wpbs.php
@@ -408,7 +408,7 @@ class WPBS {
 
 
 			if ( $image_srcset ) {
-				//echo 'imagesrcset="' . $image_srcset . '"';
+				echo 'imagesrcset="' . ( ! $webp ? $image_srcset : str_replace( [ '.jpg,.png,.jpeg' ], [ '.jpg.webp,.png.webp,.jpeg.webp' ], $image_srcset ) ) . '"';
 			}
 
 			if ( $webp ) {

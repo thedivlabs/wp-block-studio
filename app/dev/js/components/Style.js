8d273c877a683cdf83dd91bc3949165d3af09ff9
thedivlabs <121818117+thedivlabs@users.noreply.github.com>
2025-06-10 19:55:12 -0400

    Dev

--- app/dev/js/components/Style.js
+++ app/dev/js/components/Style.js
@@ -232,7 +232,6 @@ export function Style({
                 'wpbs-css': resultCss,
             });
         }
-console.log('compiling css');
         setAttributes({
             'wpbs-preload': preloadMedia
         });

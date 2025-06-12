bc768a286f65dbfd223c1b5a90f63f54d17ea562
thedivlabs <121818117+thedivlabs@users.noreply.github.com>
2025-06-12 12:57:08 -0400

    Dev

--- app/dev/blocks/figure/render.php
+++ app/dev/blocks/figure/render.php
@@ -49,34 +49,34 @@ $src_mobile_webp = $src_mobile ? $src_mobile . '.webp' : false;
 <figure <?= $wrapper_attributes ?>>
 
     <div class="wpbs-figure__media">
+        <picture>
+			<?php if ( ! empty( $src_large ) ) { ?>
+                <source type="image/webp"
+                        media="(width >= <?= $breakpoint ?>)"
+					<?= $srcset_attr . '="' . esc_attr( $src_large_webp ) . '"' ?>
+                />
+                <source type="image/jpeg"
+                        media="(width >= <?= $breakpoint ?>)"
+					<?= $srcset_attr . '="' . esc_attr( $src_large ) . '"' ?>
+                />
+			<?php } ?>
 
-		<?php if ( ! empty( $src_large ) ) { ?>
-            <source type="image/webp"
-                    media="(width >= <?= $breakpoint ?>)"
-				<?= $srcset_attr . '="' . esc_attr( $src_large_webp ) . '"' ?>
-            />
-            <source type="image/jpeg"
-                    media="(width >= <?= $breakpoint ?>)"
-				<?= $srcset_attr . '="' . esc_attr( $src_large ) . '"' ?>
-            />
-		<?php } ?>
 
+			<?php if ( ! empty( $src_mobile ) ) { ?>
+                <source type="image/webp"
+                        media="(width < <?= $breakpoint ?>)"
+					<?= $srcset_attr . '="' . esc_attr( $src_mobile_webp ) . '"' ?>
+                />
+                <source type="image/jpeg"
+                        media="(width < <?= $breakpoint ?>)"
+					<?= $srcset_attr . '="' . esc_attr( $src_mobile ) . '"' ?>
+                />
+			<?php } ?>
 
-		<?php if ( ! empty( $src_mobile ) ) { ?>
-            <source type="image/webp"
-                    media="(width < <?= $breakpoint ?>)"
-				<?= $srcset_attr . '="' . esc_attr( $src_mobile_webp ) . '"' ?>
-            />
-            <source type="image/jpeg"
-                    media="(width < <?= $breakpoint ?>)"
-				<?= $srcset_attr . '="' . esc_attr( $src_mobile ) . '"' ?>
-            />
-		<?php } ?>
-
-		<?= wp_get_attachment_image( $fallback_large_id, $settings['resolutionLarge'] ?? $settings['resolutionMobile'] ?? 'large', false, [
-			'loading' => ! empty( $settings['eager'] ) ? 'eager' : 'lazy'
-		] ) ?>
-
+			<?= wp_get_attachment_image( $fallback_large_id, $settings['resolutionLarge'] ?? $settings['resolutionMobile'] ?? 'large', false, [
+				'loading' => ! empty( $settings['eager'] ) ? 'eager' : 'lazy'
+			] ) ?>
+        </picture>
     </div>
 
 </figure>

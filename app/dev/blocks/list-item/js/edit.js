37c256809438c09e74f11b58eadf422fbadf49aa
thedivlabs <121818117+thedivlabs@users.noreply.github.com>
2025-06-12 19:25:00 -0400

    Dev

---
+++ app/dev/blocks/list-item/js/edit.js
@@ -0,0 +1,34 @@
+import '../scss/block.scss';
+
+import {
+    useBlockProps,
+    useInnerBlocksProps, InspectorControls, PanelColorSettings
+} from "@wordpress/block-editor"
+import {registerBlockType} from "@wordpress/blocks"
+import metadata from "../block.json"
+
+registerBlockType(metadata.name, {
+    apiVersion: 3,
+    attributes: {
+        ...metadata.attributes
+    },
+    edit: ({attributes, setAttributes, clientId}) => {
+
+
+        const blockProps = useBlockProps({
+            className: 'wpbs-list-item',
+        });
+
+        return <li {...useInnerBlocksProps(blockProps, {})}></li>;
+    },
+    save: (props) => {
+
+        const blockProps = useBlockProps.save({
+            className: 'wpbs-list-item',
+        });
+
+        return <li {...useInnerBlocksProps(blockProps, {})}></li>;
+    }
+})
+
+
--- app/dev/blocks/list/js/edit.js
+++ app/dev/blocks/list/js/edit.js
@@ -17,24 +17,25 @@ import {
     ColorPalette,
     BaseControl,
     __experimentalUnitControl as UnitControl,
+    __experimentalNumberControl as NumberControl,
 } from "@wordpress/components";
 import {Style, STYLE_ATTRIBUTES} from "Components/Style"
-import NumberControl from "@wordpress/components/build-types/number-control";
+import Breakpoint from "Components/Breakpoint"
 
 
 const ICON_STYLES = [
-    { label: 'Solid', value: 'fas' },
-    { label: 'Regular', value: 'far' },
-    { label: 'Light', value: 'fal' },
-    { label: 'Thin', value: 'fat' },
-    { label: 'Duotone', value: 'fad' },
-    { label: 'Brands', value: 'fab' },
+    {label: 'Solid', value: 'fas'},
+    {label: 'Regular', value: 'far'},
+    {label: 'Light', value: 'fal'},
+    {label: 'Thin', value: 'fat'},
+    {label: 'Duotone', value: 'fad'},
+    {label: 'Brands', value: 'fab'},
 ];
 
 function blockClasses(attributes = {}) {
     return [
-        'wpbs-list',
-        (attributes.className || '').split(' ').includes('is-style-image') ? 'wpbs-list--image' : null,
+        'wpbs-icon-list',
+        (attributes.className || '').split(' ').includes('is-style-image') ? 'wpbs-icon-list--image' : null,
         'w-fit max-w-full',
         attributes.uniqueId,
     ].filter(x => x).join(' ');
@@ -45,7 +46,7 @@ registerBlockType(metadata.name, {
     attributes: {
         ...metadata.attributes,
         ...STYLE_ATTRIBUTES,
-        'wpbs-list': {
+        'wpbs-icon-list': {
             type: 'object',
             default: {
                 icon: undefined,
@@ -53,14 +54,14 @@ registerBlockType(metadata.name, {
                 iconColor: undefined,
                 iconSize: undefined,
                 iconSpace: undefined,
-                columnsMobile:undefined,
-                columnsLarge:undefined,
+                columnsMobile: undefined,
+                columnsLarge: undefined,
             }
         }
     },
     edit: ({attributes, setAttributes, clientId}) => {
 
-        const uniqueId = useInstanceId(registerBlockType, 'wpbs-list');
+        const uniqueId = useInstanceId(registerBlockType, 'wpbs-icon-list');
 
         useEffect(() => {
             setAttributes({uniqueId: uniqueId});
@@ -69,12 +70,12 @@ registerBlockType(metadata.name, {
 
         const updateSettings = useCallback(({newValue}) => {
             setAttributes({
-                'wpbs-list': {
-                    ...attributes['wpbs-list'],
+                'wpbs-icon-list': {
+                    ...attributes['wpbs-icon-list'],
                     ...newValue,
                 },
             });
-        }, [attributes['wpbs-list'], setAttributes, uniqueId]);
+        }, [attributes['wpbs-icon-list'], setAttributes, uniqueId]);
 
         const blockProps = useBlockProps({
             className: blockClasses(attributes),
@@ -87,16 +88,16 @@ registerBlockType(metadata.name, {
                     <Grid columns={2} columnGap={15} rowGap={20}>
                         <NumberControl
                             label="Columns Mobile"
-                            value={attributes['wpbs-list']?.columnsMobile ?? 1}
-                            onChange={(val) => updateSettings({ columnsMobile: val })}
+                            value={attributes['wpbs-icon-list']?.columnsMobile ?? 1}
+                            onChange={(val) => updateSettings({columnsMobile: val})}
                             min={1}
                             max={3}
                             step={1}
                         />
                         <NumberControl
                             label="Columns Large"
-                            value={attributes['wpbs-list']?.columnsLarge ?? 1}
-                            onChange={(val) => updateSettings({ columnsLarge: val })}
+                            value={attributes['wpbs-icon-list']?.columnsLarge ?? 1}
+                            onChange={(val) => updateSettings({columnsLarge: val})}
                             min={1}
                             max={3}
                             step={1}
@@ -105,8 +106,8 @@ registerBlockType(metadata.name, {
 
                     <TextControl
                         label="Icon"
-                        value={attributes['wpbs-list'].icon ?? ''}
-                        onChange={(val) => updateSettings({ icon: val })}
+                        value={attributes['wpbs-icon-list'].icon ?? ''}
+                        onChange={(val) => updateSettings({icon: val})}
                     />
 
 
@@ -114,23 +115,26 @@ registerBlockType(metadata.name, {
 
                         <SelectControl
                             label="Icon Style"
-                            value={attributes['wpbs-list'].iconStyle ?? ''}
+                            value={attributes['wpbs-icon-list'].iconStyle ?? ''}
                             options={ICON_STYLES}
-                            onChange={(val) => updateSettings({ iconStyle: val })}
+                            onChange={(val) => updateSettings({iconStyle: val})}
                         />
                         <UnitControl
                             label="Icon Size"
-                            value={attributes['wpbs-list'].iconSize ?? ''}
-                            onChange={(val) => updateSettings({ iconSize: val })}
+                            value={attributes['wpbs-icon-list'].iconSize ?? ''}
+                            onChange={(val) => updateSettings({iconSize: val})}
                             units={['px', 'em', 'rem']}
                             isResettable
                         />
                         <UnitControl
                             label="Icon Space"
-                            value={attributes['wpbs-list'].iconSpace ?? ''}
-                            onChange={(val) => updateSettings({ iconSpace: val })}
-                            units={['px', 'em', 'rem','ch']}
+                            value={attributes['wpbs-icon-list'].iconSpace ?? ''}
+                            onChange={(val) => updateSettings({iconSpace: val})}
+                            units={['px', 'em', 'rem', 'ch']}
                             isResettable
+                        />
+                        <Breakpoint defaultValue={attributes['wpbs-icon-list']?.breakpoint}
+                                    callback={(newValue) => updateSettings({breakpoint: newValue})}
                         />
                     </Grid>
                     <PanelColorSettings
@@ -140,8 +144,8 @@ registerBlockType(metadata.name, {
                             {
                                 slug: 'iconColor',
                                 label: 'Icon Color',
-                                value: attributes['wpbs-list'].iconColor ?? '',
-                                onChange: (newValue) => updateSettings({ iconColor: newValue }),
+                                value: attributes['wpbs-icon-list'].iconColor ?? '',
+                                onChange: (newValue) => updateSettings({iconColor: newValue}),
                                 isShownByDefault: true,
                             }
                         ]}
@@ -149,12 +153,20 @@ registerBlockType(metadata.name, {
                 </Grid>
             </InspectorControls>
             <Style attributes={attributes} setAttributes={setAttributes}
-                   deps={['wpbs-list']}
-                   props={{}}
+                   deps={['wpbs-icon-list']}
+                   props={{
+                       '--icon': attributes['wpbs-icon-list']?.icon,
+                       '--icon-color': attributes['wpbs-icon-list']?.iconColor,
+                       '--icon-size': attributes['wpbs-icon-list']?.iconSize,
+                       '--icon-space': attributes['wpbs-icon-list']?.iconSpace,
+                       '--icon-style': attributes['wpbs-icon-list']?.iconStyle,
+                       '--list-columns': attributes['wpbs-icon-list']?.columnsMobile ?? attributes['wpbs-icon-list']?.columnsLarge ?? 1,
+                       breakpoints: {}
+                   }}
             />
 
             <ul {...useInnerBlocksProps(blockProps, {
-                allowedBlocks: ['wpbs/list-item']
+                allowedBlocks: ['wpbs/icon-list-item']
             })}></ul>
 
         </>;

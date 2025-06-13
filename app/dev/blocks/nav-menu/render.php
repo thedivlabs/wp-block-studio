403b91a8c75e5b99c62ec0cb7f6b73eddf2de334
thedivlabs <121818117+thedivlabs@users.noreply.github.com>
2025-06-12 22:10:27 -0400

    Dev

---
+++ app/dev/blocks/nav-menu/render.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$location = $attributes['location'] ?? 'primary';
+$container_class = $attributes['containerClass'] ?? 'wpbs-menu-container';
+$menu_class = $attributes['menuClass'] ?? 'wpbs-menu';
+
+$locations = get_nav_menu_locations();
+$menu_id = $locations[$location] ?? null;
+
+if (!$menu_id) {
+	echo '<div class="' . esc_attr($container_class) . '">Menu not found.</div>';
+	return;
+}
+
+echo wp_nav_menu([
+	'menu' => $menu_id,
+	'menu_class' => esc_attr($menu_class),
+	'container_class' => esc_attr($container_class),
+	'echo' => false,
+]);

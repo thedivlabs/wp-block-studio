4b4cbed33fbe83db9ebe6daf4c7f0a8c88eef57a
thedivlabs <121818117+thedivlabs@users.noreply.github.com>
2025-06-13 12:11:35 -0400

    Dev

--- app/dev/blocks/content-tabs-nav/js/edit.js
+++ app/dev/blocks/content-tabs-nav/js/edit.js
@@ -9,10 +9,9 @@ import {Style, STYLE_ATTRIBUTES} from "Components/Style.js";
 import {LayoutControls, LAYOUT_ATTRIBUTES} from "Components/Layout"
 
 import {useState, useEffect} from '@wordpress/element';
-import {InnerBlocks} from '@wordpress/block-editor';
-import {useSelect} from '@wordpress/data';
-import {store as blockEditorStore} from '@wordpress/block-editor';
-import {useMemo} from '@wordpress/element';
+import { useBlockContext } from '@wordpress/block-editor';
+import { useSelect, useDispatch } from '@wordpress/data';
+import { useCallback } from '@wordpress/element';
 import {useInstanceId} from "@wordpress/compose";
 
 
@@ -46,6 +45,14 @@ registerBlockType(metadata.name, {
             });
         }, []);
 
+        const { tabPanels = [], tabActive = null } = useBlockContext();
+
+        const { setTabActive } = useDispatch('wpbs/content-tabs');
+
+        const handleClick = useCallback((clientId) => {
+            setTabActive(clientId);
+        }, []);
+
         const blockProps = useBlockProps({
             className: classNames(attributes),
         });
@@ -57,7 +64,24 @@ registerBlockType(metadata.name, {
             <Style attributes={attributes} setAttributes={setAttributes}
                    deps={['wpbs-content-tabs-nav']}
             />
-            <nav {...blockProps}>NAVIGATION</nav>
+            <div {...blockProps} >
+                {tabPanels.map((panel) => {
+                    const isActive = panel.clientId === tabActive;
+
+                    return (
+                        <button
+                            key={panel.clientId}
+                            onClick={() => handleClick(panel.clientId)}
+                            className={classnames('wpbs-tab-button', {
+                                'is-active': isActive,
+                            })}
+                            type="button"
+                        >
+                            {panel.title}
+                        </button>
+                    );
+                })}
+            </div>
         </>;
     },
     save: (props) => {

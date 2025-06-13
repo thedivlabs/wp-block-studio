cd7b1c67edecba3d126cc22718803169a6888388
thedivlabs <121818117+thedivlabs@users.noreply.github.com>
2025-06-13 17:14:41 -0400

    Dev .wpbs-content-tabs-panel {   opacity: 0;   visibility: hidden;   pointer-events: none;   transition: opacity 0.5s ease, visibility 0s linear 0.5s;
      &.active {
        opacity: 1;
        visibility: visible;
        pointer-events: auto;
        transition: opacity 0.5s ease;
      }
    }

--- app/dev/blocks/content-tabs/js/edit.js
+++ app/dev/blocks/content-tabs/js/edit.js
@@ -15,7 +15,23 @@ import {useState, useEffect} from '@wordpress/element';
 import {select, subscribe} from '@wordpress/data';
 import {store as blockEditorStore} from '@wordpress/block-editor';
 import {useInstanceId} from "@wordpress/compose";
+import {
+    PanelBody, TabPanel,
+    __experimentalBorderControl as BorderControl,
+    __experimentalGrid as Grid,
+    __experimentalInputControl as InputControl,
+    __experimentalNumberControl as NumberControl,
+    __experimentalUnitControl as UnitControl,
+    BaseControl,
+    ToggleControl, __experimentalBoxControl as BoxControl
+} from "@wordpress/components";
+import React, {useCallback} from "react";
 
+const DIMENSION_UNITS = [
+    {value: 'px', label: 'px', default: 0},
+    {value: 'em', label: 'em', default: 0},
+    {value: 'rem', label: 'rem', default: 0},
+]
 
 function classNames(attributes = {}, editor = false) {
 
@@ -50,6 +66,18 @@ registerBlockType(metadata.name, {
                 'uniqueId': uniqueId
             });
         }, []);
+
+        const updateSettings = useCallback((newValue) => {
+            const result = {
+                ...attributes['wpbs-content-tabs'],
+                ...newValue
+            };
+
+            setAttributes({
+                'wpbs-content-tabs': result
+            });
+
+        }, [setAttributes])
 
         useEffect(() => {
 
@@ -125,8 +153,191 @@ registerBlockType(metadata.name, {
             ],
         });
 
-        return <>
+        const buttonTabOptions = <Grid columns={1} columnGap={15} rowGap={20}>
+            <PanelColorSettings
+                enableAlpha
+                className={'!p-0 !border-0 [&_.components-tools-panel-item]:!m-0'}
+                colorSettings={[
+                    {
+                        slug: 'background-color',
+                        label: 'Background Color',
+                        value: attributes['wpbs-content-tabs']?.['button-color-background'],
+                        onChange: (newValue) => updateSettings({'button-color-background': newValue}),
+                        isShownByDefault: true
+                    }
+                ]}
+            />
+            <PanelColorSettings
+                enableAlpha
+                className={'!p-0 !border-0 [&_.components-tools-panel-item]:!m-0'}
+                colorSettings={[
+                    {
+                        slug: 'text-color',
+                        label: 'Text Color',
+                        value: attributes['wpbs-content-tabs']?.['button-color-text'],
+                        onChange: (newValue) => updateSettings({'button-color-text': newValue}),
+                        isShownByDefault: true
+                    }
+                ]}
+            />
+            <BorderControl
+                __next40pxDefaultSize
+                enableAlpha
+                enableStyle
+                disableUnits
+                value={attributes['wpbs-content-tabs']?.['button-border']}
+                colors={WPBS?.settings?.colors ?? []}
+                __experimentalIsRenderedInSidebar={true}
+                label="Border"
+                onChange={(newValue) => updateSettings({'button-border': newValue})}
+                shouldSanitizeBorder
+            />
+            <BorderControl
+                __next40pxDefaultSize
+                enableAlpha
+                enableStyle
+                disableUnits
+                value={attributes['wpbs-content-tabs']?.['button-divider']}
+                colors={WPBS?.settings?.colors ?? []}
+                __experimentalIsRenderedInSidebar={true}
+                label="Divider"
+                onChange={(newValue) => updateSettings({'button-divider': newValue})}
+                shouldSanitizeBorder
+            />
+            <BoxControl
+                label={'Padding'}
+                values={attributes['wpbs-content-tabs']?.['button-padding']}
+                sides={['top', 'left']}
+                onChange={(newValue) => updateSettings({'button-padding': newValue})}
+                inputProps={{
+                    units: DIMENSION_UNITS
+                }}
+                __nextHasNoMarginBottom={true}
+            />
+        </Grid>;
+        const buttonTabHover = <Grid columns={1} columnGap={15} rowGap={20}>
+            <PanelColorSettings
+                enableAlpha
+                className={'!p-0 !border-0 [&_.components-tools-panel-item]:!m-0'}
+                colorSettings={[
+                    {
+                        slug: 'background-color-hover',
+                        label: 'Background Color',
+                        value: attributes['wpbs-content-tabs']?.['button-color-background-hover'],
+                        onChange: (newValue) => updateSettings({'button-color-background-hover': newValue}),
+                        isShownByDefault: true
+                    }
+                ]}
+            />
+            <PanelColorSettings
+                enableAlpha
+                className={'!p-0 !border-0 [&_.components-tools-panel-item]:!m-0'}
+                colorSettings={[
+                    {
+                        slug: 'text-color-hover',
+                        label: 'Text Color',
+                        value: attributes['wpbs-content-tabs']?.['button-color-text-hover'],
+                        onChange: (newValue) => updateSettings({'button-color-text-hover': newValue}),
+                        isShownByDefault: true
+                    }
+                ]}
+            />
+            <PanelColorSettings
+                enableAlpha
+                className={'!p-0 !border-0 [&_.components-tools-panel-item]:!m-0'}
+                colorSettings={[
+                    {
+                        slug: 'border-color-hover',
+                        label: 'Border Color',
+                        value: attributes['wpbs-content-tabs']?.['button-color-border-hover'],
+                        onChange: (newValue) => updateSettings({'button-color-border-hover': newValue}),
+                        isShownByDefault: true
+                    }
+                ]}
+            />
 
+        </Grid>;
+        const buttonTabActive = <Grid columns={1} columnGap={15} rowGap={20}>
+            <PanelColorSettings
+                enableAlpha
+                className={'!p-0 !border-0 [&_.components-tools-panel-item]:!m-0'}
+                colorSettings={[
+                    {
+                        slug: 'background-color-active',
+                        label: 'Background Color',
+                        value: attributes['wpbs-content-tabs']?.['button-color-background-active'],
+                        onChange: (newValue) => updateSettings({'button-color-background-active': newValue}),
+                        isShownByDefault: true
+                    }
+                ]}
+            />
+            <PanelColorSettings
+                enableAlpha
+                className={'!p-0 !border-0 [&_.components-tools-panel-item]:!m-0'}
+                colorSettings={[
+                    {
+                        slug: 'text-color-active',
+                        label: 'Text Color',
+                        value: attributes['wpbs-content-tabs']?.['button-color-text-active'],
+                        onChange: (newValue) => updateSettings({'button-color-text-active': newValue}),
+                        isShownByDefault: true
+                    }
+                ]}
+            />
+            <PanelColorSettings
+                enableAlpha
+                className={'!p-0 !border-0 [&_.components-tools-panel-item]:!m-0'}
+                colorSettings={[
+                    {
+                        slug: 'border-color-active',
+                        label: 'Border Color',
+                        value: attributes['wpbs-content-tabs']?.['button-color-border-active'],
+                        onChange: (newValue) => updateSettings({'button-color-border-active': newValue}),
+                        isShownByDefault: true
+                    }
+                ]}
+            />
+        </Grid>;
+
+
+
+        const buttonTabs = {
+            options: buttonTabOptions,
+            hover: buttonTabHover,
+            active: buttonTabActive
+        }
+
+        return <>
+            <InspectorControls group="styles">
+                <PanelBody title="Button" initialOpen={true}>
+                    <TabPanel
+                        className="wpbs-editor-tabs"
+                        activeClass="active"
+                        orientation="horizontal"
+                        initialTabName="options"
+                        tabs={[
+                            {
+                                name: 'options',
+                                title: 'Options',
+                                className: 'tab-options',
+                            },
+                            {
+                                name: 'hover',
+                                title: 'Hover',
+                                className: 'tab-hover'
+                            },
+                            {
+                                name: 'active',
+                                title: 'Active',
+                                className: 'tab-active',
+                            },
+                        ]}>
+                        {
+                            (tab) => (<>{buttonTabs[tab.name]}</>)
+                        }
+                    </TabPanel>
+                </PanelBody>
+            </InspectorControls>
             <LayoutControls attributes={attributes} setAttributes={setAttributes}/>
             <Style attributes={attributes} setAttributes={setAttributes}
                    deps={['wpbs-content-tabs']}

521fc53beee36e712893ec37c17e713d0d5e6c5d
thedivlabs <121818117+thedivlabs@users.noreply.github.com>
2025-06-12 22:09:41 -0400

    Dev

---
+++ app/dev/blocks/nav-menu/js/edit.js
@@ -0,0 +1,26 @@
+import "../scss/block.scss";
+
+import {
+    useBlockProps,
+    useInnerBlocksProps, InspectorControls, PanelColorSettings
+} from "@wordpress/block-editor"
+import {registerBlockType} from "@wordpress/blocks"
+import metadata from "../block.json"
+
+registerBlockType(metadata.name, {
+    apiVersion: 3,
+    attributes: {
+        ...metadata.attributes,
+        'wpbs-nav-menu': {
+            type: 'object'
+        }
+    },
+    edit: () => {
+        return null;
+    },
+    save: () => {
+        return null;
+    }
+})
+
+

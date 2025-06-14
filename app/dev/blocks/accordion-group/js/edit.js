4bcc733300cf2633842b2b5fae739bfb1d9fe0c6
thedivlabs <121818117+thedivlabs@users.noreply.github.com>
2025-06-14 13:11:37 -0400

    Dev

--- app/dev/blocks/accordion-group/js/edit.js
+++ app/dev/blocks/accordion-group/js/edit.js
@@ -13,7 +13,9 @@ import {LayoutControls, LAYOUT_ATTRIBUTES} from "Components/Layout"
 import {useEffect} from '@wordpress/element';
 import {useInstanceId} from "@wordpress/compose";
 import {
-    __experimentalGrid as Grid, PanelBody, TextControl, ToggleControl,
+    __experimentalUnitControl as UnitControl,
+    __experimentalBorderControl as BorderControl, __experimentalBoxControl as BoxControl,
+    __experimentalGrid as Grid, PanelBody, TabPanel, TextControl, ToggleControl,
 } from "@wordpress/components";
 import React, {useCallback} from "react";
 
@@ -28,6 +30,12 @@ function classNames(attributes = {}) {
     return [
         'wpbs-accordion-group',
         'w-full relative',
+        attributes['wpbs-accordion-group']?.['icon-animate'] === false ? '--static-icon' : null,
+        !!attributes['wpbs-accordion-group']?.['hide-icon'] ? '--no-icon' : null,
+        !!attributes['wpbs-accordion-group']?.['header-color-hover'] ? '--header-hover' : null,
+        !!attributes['wpbs-accordion-group']?.['header-text-color-hover'] ? '--header-text-hover' : null,
+        !!attributes['wpbs-accordion-group']?.['header-color-active'] ? '--header-active' : null,
+        !!attributes['wpbs-accordion-group']?.['header-text-color-active'] ? '--header-text-active' : null,
         attributes.uniqueId,
     ].filter(x => x).join(' ');
 }
@@ -78,21 +86,187 @@ registerBlockType(metadata.name, {
             ],
         });
 
+        const tabOptions = <Grid columns={1} columnGap={15} rowGap={20}>
+            <Grid columns={2} columnGap={15} rowGap={20}>
+                <TextControl
+                    label={'Icon Closed'}
+                    value={attributes['wpbs-accordion-group']?.['icon-closed']}
+                    onChange={(newValue) => updateSettings({'icon-closed': newValue})}
+                    __nextHasNoMarginBottom
+                />
+                <TextControl
+                    label={'Icon Open'}
+                    value={attributes['wpbs-accordion-group']?.['icon-open']}
+                    onChange={(newValue) => updateSettings({'icon-open': newValue})}
+                    __nextHasNoMarginBottom
+                />
+                <UnitControl
+                    label="Icon Size"
+                    value={attributes['wpbs-accordion-group']?.['icon-size'] ?? ''}
+                    onChange={(val) => updateSettings({'icon-size': val})}
+                    units={DIMENSION_UNITS}
+                    isResetValueOnUnitChange={true}
+                    __next40pxDefaultSize
+                    __nextHasNoMarginBottom
+                />
+            </Grid>
+            <Grid columns={2} columnGap={15} rowGap={20}>
+                <ToggleControl
+                    label={'Animate Icon'}
+                    value={attributes['wpbs-accordion-group']?.['icon-animate'] !== false}
+                    onChange={(newValue) => updateSettings({'icon-animate': newValue})}
+                    __nextHasNoMarginBottom
+                />
+                <ToggleControl
+                    label={'Hide Icon'}
+                    value={!!attributes['wpbs-accordion-group']?.['icon-hide']}
+                    onChange={(newValue) => updateSettings({'icon-hide': newValue})}
+                    __nextHasNoMarginBottom
+                />
+            </Grid>
+            <Grid columns={1} columnGap={0} rowGap={0}>
+                <PanelColorSettings
+                    enableAlpha
+                    className={'!p-0 !border-0 [&_.components-tools-panel-item]:!m-0'}
+                    colorSettings={[
+                        {
+                            slug: 'icon-color',
+                            label: 'Icon Color',
+                            value: attributes['wpbs-accordion-group']?.['icon-color'],
+                            onChange: (newValue) => updateSettings({'icon-color': newValue}),
+                            isShownByDefault: true
+                        }
+                    ]}
+                />
+            </Grid>
+        </Grid>;
+
+        const tabHover = <Grid columns={1} columnGap={0} rowGap={0}>
+
+            <PanelColorSettings
+                enableAlpha
+                className={'!p-0 !border-0 [&_.components-tools-panel-item]:!m-0'}
+                colorSettings={[
+                    {
+                        slug: 'icon-color-hover',
+                        label: 'Icon Color',
+                        value: attributes['wpbs-accordion-group']?.['icon-color-hover'],
+                        onChange: (newValue) => updateSettings({'icon-color-hover': newValue}),
+                        isShownByDefault: true
+                    }
+                ]}
+            />
+            <PanelColorSettings
+                enableAlpha
+                className={'!p-0 !border-0 [&_.components-tools-panel-item]:!m-0'}
+                colorSettings={[
+                    {
+                        slug: 'header-color-hover',
+                        label: 'Header Color',
+                        value: attributes['wpbs-accordion-group']?.['header-color-hover'],
+                        onChange: (newValue) => updateSettings({'header-color-hover': newValue}),
+                        isShownByDefault: true
+                    }
+                ]}
+            />
+            <PanelColorSettings
+                enableAlpha
+                className={'!p-0 !border-0 [&_.components-tools-panel-item]:!m-0'}
+                colorSettings={[
+                    {
+                        slug: 'header-text-color-hover',
+                        label: 'Header Text Color',
+                        value: attributes['wpbs-accordion-group']?.['header-text-color-hover'],
+                        onChange: (newValue) => updateSettings({'header-text-color-hover': newValue}),
+                        isShownByDefault: true
+                    }
+                ]}
+            />
+        </Grid>;
+
+        const tabActive = <Grid columns={1} columnGap={0} rowGap={0}>
+
+            <PanelColorSettings
+                enableAlpha
+                className={'!p-0 !border-0 [&_.components-tools-panel-item]:!m-0'}
+                colorSettings={[
+                    {
+                        slug: 'icon-color-active',
+                        label: 'Icon Color',
+                        value: attributes['wpbs-accordion-group']?.['icon-color-active'],
+                        onChange: (newValue) => updateSettings({'icon-color-active': newValue}),
+                        isShownByDefault: true
+                    }
+                ]}
+            />
+            <PanelColorSettings
+                enableAlpha
+                className={'!p-0 !border-0 [&_.components-tools-panel-item]:!m-0'}
+                colorSettings={[
+                    {
+                        slug: 'header-color-active',
+                        label: 'Header Color',
+                        value: attributes['wpbs-accordion-group']?.['header-color-active'],
+                        onChange: (newValue) => updateSettings({'header-color-active': newValue}),
+                        isShownByDefault: true
+                    }
+                ]}
+            />
+            <PanelColorSettings
+                enableAlpha
+                className={'!p-0 !border-0 [&_.components-tools-panel-item]:!m-0'}
+                colorSettings={[
+                    {
+                        slug: 'header-text-color-active',
+                        label: 'Header Text Color',
+                        value: attributes['wpbs-accordion-group']?.['header-text-color-active'],
+                        onChange: (newValue) => updateSettings({'header-text-color-active': newValue}),
+                        isShownByDefault: true
+                    }
+                ]}
+            />
+        </Grid>;
+
+        const accordionTabs = {
+            options: tabOptions,
+            hover: tabHover,
+            active: tabActive
+        }
+
+
+        const iconOpen = attributes['wpbs-accordion-group']?.['icon-open']?.match(/^[a-fA-F0-9]{4,6}$/) ? attributes['wpbs-accordion-group']?.['icon-open'] : 'f00c';
+        const iconClosed = attributes['wpbs-accordion-group']?.['icon-closed']?.match(/^[a-fA-F0-9]{4,6}$/) ? attributes['wpbs-accordion-group']?.['icon-closed'] : 'f00c';
+
         return <>
 
             <InspectorControls group="styles">
-
-                <PanelBody title="Options" initialOpen={true}>
-                    <Grid columns={1} columnGap={15} rowGap={0}>
-                        <Grid columns={2} columnGap={15} rowGap={20}>
-                            <TextControl
-                                label={'Icon'}
-                                value={attributes['wpbs-accordion-group']?.['icon']}
-                                onChange={(newValue) => updateSettings({'icon': newValue})}
-                                __nextHasNoMarginBottom
-                            />
-                        </Grid>
-                    </Grid>
+                <PanelBody title="Button" initialOpen={true}>
+                    <TabPanel
+                        className="wpbs-editor-tabs"
+                        activeClass="active"
+                        orientation="horizontal"
+                        initialTabName="options"
+                        tabs={[
+                            {
+                                name: 'options',
+                                title: 'Options',
+                                className: 'tab-options',
+                            },
+                            {
+                                name: 'hover',
+                                title: 'Hover',
+                                className: 'tab-hover'
+                            },
+                            {
+                                name: 'active',
+                                title: 'Active',
+                                className: 'tab-active',
+                            },
+                        ]}>
+                        {
+                            (tab) => (<>{accordionTabs[tab.name]}</>)
+                        }
+                    </TabPanel>
                 </PanelBody>
             </InspectorControls>
 
@@ -100,6 +274,18 @@ registerBlockType(metadata.name, {
             <LayoutControls attributes={attributes} setAttributes={setAttributes}/>
             <Style attributes={attributes} setAttributes={setAttributes}
                    deps={['wpbs-accordion-group']}
+                   props={{
+                       '--icon-open': `"\\${iconOpen}"`,
+                       '--icon-closed': `"\\${iconClosed}"`,
+                       '--icon-size': attributes['wpbs-accordion-group']?.['icon-size'],
+                       '--icon-color': attributes['wpbs-accordion-group']?.['icon-color'],
+                       '--icon-color-hover': attributes['wpbs-accordion-group']?.['icon-color-hover'],
+                       '--header-color-hover': attributes['wpbs-accordion-group']?.['header-color-hover'],
+                       '--header-text-color-hover': attributes['wpbs-accordion-group']?.['header-text-color-hover'],
+                       '--icon-color-active': attributes['wpbs-accordion-group']?.['icon-color-active'],
+                       '--header-color-active': attributes['wpbs-accordion-group']?.['header-color-active'],
+                       '--text-color-active': attributes['wpbs-accordion-group']?.['text-color-active'],
+                   }}
             />
 
             <div {...innerBlocksProps} />

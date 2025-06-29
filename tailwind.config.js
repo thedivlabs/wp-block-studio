db552b6b505b588cefabf553d4306d52f2d91369
Alex Ferrao <alexsr20det@gmail.com>
2025-06-29 13:10:26 -0400

    Dev

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: [
+        './app/dev/**/*.{js,jsx,ts,tsx,css,scss,php,html}',
+        './app/public/wp-content/themes/wp-block-studio/*.{php,html}',
+        './app/public/wp-content/themes/wp-block-studio/core/**/*.{js,jsx,ts,tsx,css,scss,php,html}',
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};

994863b70a2e4338cef3e394870f362e42da7122
thedivlabs <121818117+thedivlabs@users.noreply.github.com>
2025-07-22 12:39:14 -0400

    Dev

--- app/dev/blocks/masthead/render.php
+++ app/dev/blocks/masthead/render.php
@@ -5,12 +5,15 @@ add_filter( 'wpbs_critical_css', function ( $css_array ) {
 	$css_array['wpbs-site-header-settings'] = '';
 
 	$breakpoints    = wp_get_global_settings()['custom']['breakpoints'] ?? [];
-	$header_options = wp_get_global_settings()['custom']['header']['height'] ?? [];
+	$header_height = wp_get_global_settings()['custom']['header']['height'] ?? [];
 
 
 	$css_array['wpbs-site-header-settings'] .= ':root {';
+	$css_array['wpbs-site-header-settings'] .= '--wpbs-header-height: ' . $header_height['xs'] . ';';
 
-	foreach ( $header_options as $bp => $height ) {
+	foreach ( array_filter($header_height, function($bp){
+		return $bp != 'xs';
+	}, ARRAY_FILTER_USE_KEY) as $bp => $height ) {
 		$css_array['wpbs-site-header-settings'] .= '@media (min-width: ' . $breakpoints[ $bp ] . '){--wpbs-header-height: ' . $height . '}';
 	}
 

b4fb76cace9015160f135c2ed737e139f3fb6ae6
thedivlabs <121818117+thedivlabs@users.noreply.github.com>
2025-08-02 19:38:52 -0400

    Dev

--- app/dev/blocks/nav-menu/view.js
+++ app/dev/blocks/nav-menu/view.js
@@ -12,38 +12,26 @@ const {state} = store('wpbs/nav-menu', {
 
             [...element.querySelectorAll('li:has(> .sub-menu)')].forEach((menu_item) => {
 
-                const menu = menu_item.closest('.wpbs-nav-menu');
-
                 const link = menu_item.querySelector(':scope > a');
-                const sub_menu = menu_item.querySelector(':scope > ul.sub-menu');
+                const sub_menu = menu_item.querySelector(':scope > .sub-menu');
 
                 if (!is_accordion) {
                     menu_item.addEventListener('mouseenter', (e) => {
-                        if (window.matchMedia("(min-width: 720px)")) {
-                            const bounds = sub_menu.getBoundingClientRect();
-                            const width = sub_menu.offsetWidth;
+                        const bounds = sub_menu.getBoundingClientRect();
+                        const width = sub_menu.offsetWidth;
 
-                            if (bounds.left + width > window.outerWidth) {
-                                sub_menu.classList.add('offset-right');
-                            } else {
-                                sub_menu.classList.remove('offset-right');
-                            }
-                            if (bounds.left < 0) {
-                                sub_menu.classList.add('offset-left');
-                            } else {
-                                sub_menu.classList.remove('offset-left');
-                            }
+                        if (bounds.left + width > window.outerWidth) {
+                            sub_menu.classList.add('offset-right');
+                        } else {
+                            sub_menu.classList.remove('offset-right');
+                        }
+                        if (bounds.left < 0) {
+                            sub_menu.classList.add('offset-left');
+                        } else {
+                            sub_menu.classList.remove('offset-left');
                         }
                     }, {
                         passive: true
-                    });
-                }
-
-                if (is_accordion && link) {
-                    link.addEventListener('click', (e) => {
-                        e.preventDefault();
-                        $(sub_menu).slideToggle('fast');
-                        $(menu_item).toggleClass('active');
                     });
                 }
 

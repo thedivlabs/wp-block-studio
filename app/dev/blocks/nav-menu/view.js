ecba07087f0433aa150440d2d3d23230c03bc3e3
Alex Ferrao <alexsr20det@gmail.com>
2025-08-03 09:15:32 -0400

    Dev

--- app/dev/blocks/nav-menu/view.js
+++ app/dev/blocks/nav-menu/view.js
@@ -9,13 +9,16 @@ function initDropdown(element) {
 
         const sub_menu = menu_item.querySelector(':scope > .sub-menu');
 
+        if (!sub_menu) {
+            return;
+        }
+
         sub_menu.addEventListener('transitionend', (e) => {
 
             const opacity = window.getComputedStyle(sub_menu).opacity;
 
             if (parseFloat(opacity) < .1) {
-                sub_menu.classList.remove('offset-right');
-                sub_menu.classList.remove('offset-left');
+                sub_menu.classList.remove('offset-left', 'offset-right');
             }
 
         }, {
@@ -27,7 +30,9 @@ function initDropdown(element) {
             const bounds = sub_menu.getBoundingClientRect();
             const width = sub_menu.offsetWidth;
 
-            if (bounds.left + width > window.outerWidth) {
+            sub_menu.classList.remove('offset-left', 'offset-right');
+
+            if (bounds.left + width > window.innerWidth) {
                 sub_menu.classList.add('offset-right');
             }
             if (bounds.left < 0) {

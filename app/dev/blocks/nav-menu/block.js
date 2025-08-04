a7527375f7ff7d33d193de530f9df6ad5659809f
Alex Ferrao <alexsr20det@gmail.com>
2025-08-03 20:06:56 -0400

    Dev

--- app/dev/blocks/nav-menu/block.js
+++ app/dev/blocks/nav-menu/block.js
@@ -12,20 +12,25 @@ import {useInstanceId} from '@wordpress/compose';
 import React, {useCallback, useMemo} from "react";
 import {useSelect} from "@wordpress/data";
 import {
-    __experimentalBorderControl as BorderControl,
+    __experimentalBorderControl as BorderControl, __experimentalBoxControl as BoxControl,
     __experimentalGrid as Grid,
-    __experimentalUnitControl as UnitControl,
+    __experimentalUnitControl as UnitControl, BaseControl,
     PanelBody,
     SelectControl,
     TabPanel, TextControl,
     ToggleControl
 } from "@wordpress/components";
 import {useSetting} from '@wordpress/block-editor';
+import {DIMENSION_UNITS, DIMENSION_UNITS_TEXT} from "Includes/config";
 
 
 function blockClassNames(attributes = {}) {
+
+    const {'wpbs-nav-menu': settings = {}} = attributes;
+
     return [
         'wpbs-nav-menu wpbs-has-container flex flex-wrap',
+        !!settings?.['submenu-fade'] ? '--fade' : null,
         attributes?.uniqueId ?? null
     ].filter(x => x).join(' ');
 }
@@ -76,7 +81,31 @@ registerBlockType(metadata.name, {
         }, [settings, setAttributes]);
 
         const cssProps = useMemo(() => {
-            return {};
+            return Object.fromEntries(Object.entries({
+                '--icon': settings?.['icon'] ?? null,
+                '--icon-space': settings?.['icon-space'] ?? null,
+                '--color-background-active': settings?.['color-background-active'] ?? null,
+                '--color-text-active': settings?.['color-text-active'] ?? null,
+                '--link-padding': settings?.['link-padding'] ?? null,
+                '--divider': settings?.['divider'] ?? null,
+                '--submenu-space': settings?.['submenu-space'] ?? null,
+                '--submenu-rounded': settings?.['submenu-rounded'] ?? null,
+                '--submenu-padding': settings?.['submenu-padding'] ?? null,
+                '--submenu-gap': settings?.['submenu-gap'] ?? null,
+                '--submenu-icon': settings?.['submenu-icon'] ?? null,
+                '--submenu-icon-space': settings?.['submenu-icon-space'] ?? null,
+                '--submenu-color-background': settings?.['submenu-color-background'] ?? null,
+                '--submenu-color-background-hover': settings?.['submenu-color-background-hover'] ?? null,
+                '--submenu-color-text-hover': settings?.['submenu-color-text-hover'] ?? null,
+                '--submenu-color-text': settings?.['submenu-color-text'] ?? null,
+                '--submenu-color-icon': settings?.['submenu-color-icon'] ?? null,
+                '--submenu-color-divider-hover': settings?.['submenu-color-divider-hover'] ?? null,
+                '--submenu-color-background-active': settings?.['submenu-color-background-active'] ?? null,
+                '--submenu-color-text-active': settings?.['submenu-color-text-active'] ?? null,
+                '--submenu-link-padding': settings?.['submenu-link-padding'] ?? null,
+                '--submenu-border': settings?.['submenu-border'] ?? null,
+                '--submenu-divider': settings?.['submenu-divider'] ?? null,
+            }).filter(x => !!x))
         }, [settings]);
 
         const blockProps = useBlockProps({
@@ -101,19 +130,51 @@ registerBlockType(metadata.name, {
                     value={settings?.['icon'] ?? ''}
                     onChange={(newValue) => updateSettings({'icon': newValue})}
                 />
+                <UnitControl
+                    label="Icon Space"
+                    value={settings?.['icon-space'] ?? ''}
+                    onChange={(newValue) => updateSettings({'icon-space': newValue})}
+                    units={DIMENSION_UNITS_TEXT}
+                    isResetValueOnUnitChange={true}
+                    __next40pxDefaultSize
+                    __nextHasNoMarginBottom
+                />
             </Grid>
-            <PanelColorSettings
-                enableAlpha
-                className={'!p-0 !border-0 [&_.components-tools-panel-item]:!m-0'}
-                colorSettings={[
-                    {
-                        slug: 'color-current',
-                        label: 'Current',
-                        value: settings?.['color-current'],
-                        onChange: (newValue) => updateSettings({'color-current': newValue}),
-                        isShownByDefault: true
-                    }
-                ]}
+            <BaseControl label={'Colors'}>
+                <PanelColorSettings
+                    enableAlpha
+                    className={'!p-0 !border-0 [&_.components-tools-panel-item]:!m-0'}
+                    colorSettings={[
+                        {
+                            slug: 'color-background-active',
+                            label: 'Background Active',
+                            value: settings?.['color-background-active'],
+                            onChange: (newValue) => updateSettings({'color-background-active': newValue}),
+                            isShownByDefault: true
+                        },
+                        {
+                            slug: 'color-active-text',
+                            label: 'Text Active',
+                            value: settings?.['color-text-active'],
+                            onChange: (newValue) => updateSettings({'color-text-active': newValue}),
+                            isShownByDefault: true
+                        },
+                        {
+                            slug: 'color-icon',
+                            label: 'Icon',
+                            value: settings?.['color-icon'],
+                            onChange: (newValue) => updateSettings({'color-icon': newValue}),
+                            isShownByDefault: true
+                        }
+                    ]}
+                />
+            </BaseControl>
+            <BoxControl
+                label={'Link Padding'}
+                value={settings?.['link-padding'] ?? ''}
+                sides={['top', 'left']}
+                onChange={(newValue) => updateSettings({'link-padding': newValue})}
+                __nextHasNoMarginBottom={true}
             />
             <BorderControl
                 __next40pxDefaultSize
@@ -142,6 +203,44 @@ registerBlockType(metadata.name, {
 
         const tabSubMenu = <Grid columns={1} columnGap={15} rowGap={15}>
             <Grid columns={2} columnGap={15} rowGap={15}>
+
+                <UnitControl
+                    label="Space"
+                    value={settings?.['submenu-space'] ?? ''}
+                    onChange={(newValue) => updateSettings({'submenu-space': newValue})}
+                    units={DIMENSION_UNITS_TEXT}
+                    isResetValueOnUnitChange={true}
+                    __next40pxDefaultSize
+                    __nextHasNoMarginBottom
+                />
+                <UnitControl
+                    label="Rounded"
+                    value={settings?.['submenu-rounded'] ?? ''}
+                    onChange={(newValue) => updateSettings({'submenu-rounded': newValue})}
+                    units={DIMENSION_UNITS_TEXT}
+                    isResetValueOnUnitChange={true}
+                    __next40pxDefaultSize
+                    __nextHasNoMarginBottom
+                />
+                <UnitControl
+                    label="Padding"
+                    value={settings?.['submenu-padding'] ?? ''}
+                    onChange={(newValue) => updateSettings({'submenu-padding': newValue})}
+                    units={DIMENSION_UNITS_TEXT}
+                    isResetValueOnUnitChange={true}
+                    __next40pxDefaultSize
+                    __nextHasNoMarginBottom
+                />
+                <UnitControl
+                    label="Gap"
+                    value={settings?.['submenu-gap'] ?? ''}
+                    onChange={(newValue) => updateSettings({'submenu-gap': newValue})}
+                    units={DIMENSION_UNITS_TEXT}
+                    isResetValueOnUnitChange={true}
+                    __next40pxDefaultSize
+                    __nextHasNoMarginBottom
+                />
+
                 <TextControl
                     __nextHasNoMarginBottom
                     __next40pxDefaultSize
@@ -149,54 +248,81 @@ registerBlockType(metadata.name, {
                     value={settings?.['submenu-icon'] ?? ''}
                     onChange={(newValue) => updateSettings({'submenu-icon': newValue})}
                 />
+                <UnitControl
+                    label="Icon Space"
+                    value={settings?.['submenu-icon-space'] ?? ''}
+                    onChange={(newValue) => updateSettings({'submenu-icon-space': newValue})}
+                    units={DIMENSION_UNITS_TEXT}
+                    isResetValueOnUnitChange={true}
+                    __next40pxDefaultSize
+                    __nextHasNoMarginBottom
+                />
+
             </Grid>
-            <PanelColorSettings
-                enableAlpha
-                className={'!p-0 !border-0 [&_.components-tools-panel-item]:!m-0'}
-                colorSettings={[
-                    {
-                        slug: 'color-submenu-background',
-                        label: 'Background',
-                        value: settings?.['color-submenu-background'],
-                        onChange: (newValue) => updateSettings({'submenu-color-background': newValue}),
-                        isShownByDefault: true
-                    },
-                    {
-                        slug: 'color-submenu-background-hover',
-                        label: 'Background Hover',
-                        value: settings?.['color-submenu-background-hover'],
-                        onChange: (newValue) => updateSettings({'color-submenu-background-hover': newValue}),
-                        isShownByDefault: true
-                    },
-                    {
-                        slug: 'color-submenu-text',
-                        label: 'Text',
-                        value: settings?.['color-submenu-text'],
-                        onChange: (newValue) => updateSettings({'color-submenu-text': newValue}),
-                        isShownByDefault: true
-                    },
-                    {
-                        slug: 'color-submenu-text-hover',
-                        label: 'Text Hover',
-                        value: settings?.['color-submenu-text-hover'],
-                        onChange: (newValue) => updateSettings({'color-submenu-text-hover': newValue}),
-                        isShownByDefault: true
-                    },
-                    {
-                        slug: 'color-submenu-border-hover',
-                        label: 'Border Hover',
-                        value: settings?.['color-submenu-border-hover'],
-                        onChange: (newValue) => updateSettings({'color-submenu-border-hover': newValue}),
-                        isShownByDefault: true
-                    },
-                    {
-                        slug: 'color-submenu-divider-hover',
-                        label: 'Divider Hover',
-                        value: settings?.['color-submenu-divider-hover'],
-                        onChange: (newValue) => updateSettings({'color-submenu-divider-hover': newValue}),
-                        isShownByDefault: true
-                    }
-                ]}
+            <BaseControl label={'Colors'}>
+                <PanelColorSettings
+                    enableAlpha
+                    className={'!p-0 !border-0 [&_.components-tools-panel-item]:!m-0'}
+                    colorSettings={[
+                        {
+                            slug: 'color-submenu-background',
+                            label: 'Background',
+                            value: settings?.['color-submenu-background'],
+                            onChange: (newValue) => updateSettings({'color-submenu-background': newValue}),
+                            isShownByDefault: true
+                        },
+                        {
+                            slug: 'color-submenu-background-hover',
+                            label: 'Background Hover',
+                            value: settings?.['color-submenu-background-hover'],
+                            onChange: (newValue) => updateSettings({'color-submenu-background-hover': newValue}),
+                            isShownByDefault: true
+                        },
+                        {
+                            slug: 'color-submenu-text',
+                            label: 'Text',
+                            value: settings?.['color-submenu-text'],
+                            onChange: (newValue) => updateSettings({'color-submenu-text': newValue}),
+                            isShownByDefault: true
+                        },
+                        {
+                            slug: 'color-submenu-text-hover',
+                            label: 'Text Hover',
+                            value: settings?.['color-submenu-text-hover'],
+                            onChange: (newValue) => updateSettings({'color-submenu-text-hover': newValue}),
+                            isShownByDefault: true
+                        },
+                        {
+                            slug: 'color-submenu-icon',
+                            label: 'Icon',
+                            value: settings?.['color-submenu-icon'],
+                            onChange: (newValue) => updateSettings({'color-submenu-icon': newValue}),
+                            isShownByDefault: true
+                        },
+                        {
+                            slug: 'color-submenu-background-active',
+                            label: 'Background Active',
+                            value: settings?.['color-submenu-background-active'],
+                            onChange: (newValue) => updateSettings({'color-submenu-background-active': newValue}),
+                            isShownByDefault: true
+                        },
+                        {
+                            slug: 'color-submenu-text-active',
+                            label: 'Text Active',
+                            value: settings?.['color-submenu-text-active'],
+                            onChange: (newValue) => updateSettings({'color-submenu-text-active': newValue}),
+                            isShownByDefault: true
+                        }
+                    ]}
+                />
+
+            </BaseControl>
+            <BoxControl
+                label={'Link Padding'}
+                value={settings?.['submenu-link-padding'] ?? ''}
+                sides={['top', 'left']}
+                onChange={(newValue) => updateSettings({'submenu-link-padding': newValue})}
+                __nextHasNoMarginBottom={true}
             />
             <BorderControl
                 __next40pxDefaultSize

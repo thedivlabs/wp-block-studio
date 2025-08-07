f5078fe14f22ff35f15bf92b7df709a50898c864
Alex Ferrao <alexsr20det@gmail.com>
2025-08-07 19:59:08 -0400

    Dev

--- app/public/wp-content/themes/wp-block-studio/core/class-wpbs.php
+++ app/public/wp-content/themes/wp-block-studio/core/class-wpbs.php
@@ -170,6 +170,7 @@ class WPBS {
 		require_once $core_path . 'modules/class-wpbs-media-gallery.php';
 		require_once $core_path . 'modules/class-wpbs-company.php';
 		require_once $core_path . 'modules/class-wpbs-google-places.php';
+		require_once $core_path . 'modules/class-wpbs-gravity-forms.php';
 
 		WPBS_WP::init();
 		WPBS_ACF::init();
@@ -178,6 +179,7 @@ class WPBS {
 		WPBS_Media_Gallery::init();
 		WPBS_Company::init();
 		WPBS_Google_Places::init();
+		WPBS_Gravity_Forms::init();
 
 		self::init_classes( 'core/components' );
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,24 @@ module.exports = {
         './app/public/wp-content/themes/wp-block-studio/core/**/*.{js,jsx,ts,tsx,css,scss,php,html}',
     ],
     theme: {
-        extend: {},
+        extend: {
+            animation: {
+                wiggle: 'wiggle .2s ease-in-out 2',
+                marquee: 'marquee 25s linear infinite',
+            },
+            keyframes: {
+                wiggle: {
+                    '25%': {transform: 'translate(-2px,0)'},
+                    '50%': {transform: 'translate(0,0)'},
+                    '75%': {transform: 'translate(2px,0)'},
+                    '100%': {transform: 'translate(0,0)'},
+                },
+                marquee: {
+                    from: {transform: 'translateX(0)'},
+                    to: {transform: 'translateX(-100%)'},
+                }
+            },
+        },
     },
     plugins: [],
 };

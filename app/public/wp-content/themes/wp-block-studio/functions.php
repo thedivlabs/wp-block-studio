8509fd797c8171918fdaf9259873a684fbdbb8ee
Alex Ferrao <alexsr20det@gmail.com>
2025-08-08 08:26:00 -0400

    Dev

--- app/public/wp-content/themes/wp-block-studio/functions.php
+++ app/public/wp-content/themes/wp-block-studio/functions.php
@@ -1,20 +1,2 @@
 <?php
 
-// If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
-}
-
-require_once __DIR__ . '/core/class-wpbs.php';
-
-if ( class_exists( 'WPBS' ) ) {
-	$wpbs = WPBS::init();
-}
-
-
-add_action( 'admin_enqueue_scripts', function () {
-	wp_enqueue_code_editor( [
-		'type' => 'text/javascript'
-	] );
-	wp_enqueue_style( 'wp-codemirror' );
-} );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,8 @@ module.exports = {
     content: [
         './app/dev/*.{js,jsx,ts,tsx,css,scss,php,html}',
         './app/dev/**/*.{js,jsx,ts,tsx,css,scss,php,html}',
-        './app/public/wp-content/themes/wp-block-studio/*.{php,html}',
-        './app/public/wp-content/themes/wp-block-studio/core/**/*.{js,jsx,ts,tsx,css,scss,php,html}',
+        './app/public/wp-content/plugins/wp-block-studio/*.{php,html}',
+        './app/public/wp-content/plugins/wp-block-studio/core/**/*.{js,jsx,ts,tsx,css,scss,php,html}',
     ],
     theme: {
         extend: {

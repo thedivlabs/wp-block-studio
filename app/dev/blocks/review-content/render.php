f309b7dbca3383cec14b357eafc9a72dc779ae76
Alex Ferrao <alexsr20det@gmail.com>
2025-08-11 00:40:23 -0400

    Dev

--- app/dev/blocks/review-content/render.php
+++ app/dev/blocks/review-content/render.php
@@ -21,6 +21,7 @@ $review_content = match ( $style ) {
 	'rating' => get_comment_meta( $comment->comment_ID ?? false, 'rating', true ),
 	'time' => get_comment_meta( $comment->comment_ID ?? false, 'timestamp', true ),
 	'content' => $comment['comment_content'] ?? false,
+	default => false
 }
 
 ?>

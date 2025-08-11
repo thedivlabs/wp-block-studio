12ce7bd059380d259ff2938b00ad5f3be550fdf6
Alex Ferrao <alexsr20det@gmail.com>
2025-08-11 01:02:48 -0400

    Dev

--- app/dev/blocks/review-gallery/render.php
+++ app/dev/blocks/review-gallery/render.php
@@ -18,10 +18,13 @@
  * */
 
 
-$card_block = WPBS::get_block_template( array_filter( $block->parsed_block['innerBlocks'] ?? [], function ( $inner_block ) {
+$card_block = WPBS::get_block_template( array_values( array_filter( $block->parsed_block['innerBlocks'] ?? [], function ( $inner_block ) {
 	return $inner_block['blockName'] === 'wpbs/review-card';
-} )[0] ?? false );
+} ) )[0] ?? false );
 
+$nav_block = array_values( array_filter( $block->parsed_block['innerBlocks'] ?? [], function ( $inner_block ) {
+	return $inner_block['blockName'] === 'wpbs/slider-navigation';
+} ) )[0] ?? false;
 
 $company_id = $attributes['wpbs-review-gallery']['company_id'] ?? false;
 
@@ -54,23 +57,30 @@ $reviews = get_comments( array_filter( [
 ?>
 
 <div <?php echo $wrapper_attributes ?>>
+    <div class="swiper-wrapper">
+		<?php
+
+		foreach ( $reviews as $k => $reviews_item ) {
+			//WPBS::console_log( $reviews_item );
+			$block_template                      = $card_block;
+			$block_template['attrs']['uniqueId'] = $card_block['attrs']['uniqueId'] ?? '';
+			$block_template['attrs']['review']   = $reviews_item;
+
+			$new_block = new WP_Block( $block_template, array_filter( [
+				'wpbs/review' => $reviews_item,
+			] ) );
+
+			echo $new_block->render();
+
+		}
+
+		?>
+    </div>
 
 	<?php
-
-	foreach ( $reviews as $k => $reviews_item ) {
-		//WPBS::console_log( $reviews_item );
-		$block_template                      = $card_block;
-		$block_template['attrs']['uniqueId'] = $card_block['attrs']['uniqueId'] ?? '';
-		$block_template['attrs']['review']   = $reviews_item;
-
-		$new_block = new WP_Block( $block_template, array_filter( [
-			'wpbs/review' => $reviews_item,
-		] ) );
-
-		echo $new_block->render();
-
+	if ( $nav_block ) {
+		echo ( new WP_Block( $nav_block ) )->render();
 	}
-
 	?>
 
 </div>

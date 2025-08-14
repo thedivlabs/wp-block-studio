69eb66b1198f319dcfc9ebb79b1ebb21b298b69b
Alex Ferrao <alexsr20det@gmail.com>
2025-08-14 11:34:11 -0400

    Dev

--- app/dev/blocks/video/block.js
+++ app/dev/blocks/video/block.js
@@ -282,7 +282,7 @@ registerBlockType(metadata.name, {
                         </div>
                     )}
                     <div
-                        className={'wpbs-video__button pointer-events-none flex justify-center items-center absolute top-1/2 left-1/2 aspect-square z-20 transition-colors duration-300 leading-none opacity-50'}>
+                        className={'wpbs-video__button pointer-events-none flex justify-center items-center absolute top-1/2 left-1/2 aspect-square z-20 transition-colors duration-300 leading-none'}>
                         <span className={'screen-reader-text'}>Play video</span>
                     </div>
                     <img src={posterSrc}

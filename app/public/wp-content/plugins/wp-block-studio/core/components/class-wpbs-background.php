5fb09170d7a39df3ebc619938c7c288c3769b1bb
Alex Ferrao <alexsr20det@gmail.com>
2025-08-14 13:38:13 -0400

    Dev

--- app/public/wp-content/plugins/wp-block-studio/core/components/class-wpbs-background.php
+++ app/public/wp-content/plugins/wp-block-studio/core/components/class-wpbs-background.php
@@ -16,11 +16,24 @@ class WPBS_Background {
 
 		if ( ( $attributes['wpbs-background']['type'] ?? false ) == 'featured-image' ) {
 
-			$img_id_large  = get_post_thumbnail_id( get_the_ID() ) ?: ( $attributes['wpbs-background']['largeImage']['id'] ?? $attributes['wpbs-background']['mobileImage']['id'] ?? false );
-			$img_src_large = wp_get_attachment_image_src( $img_id_large, $attributes['wpbs-background']['resolution'] ?? 'large' )[0] ?? '#';
+			if ( is_tax() ) {
+				$term = get_queried_object(); // Current term object
 
-			$img_id_mobile  = get_field( 'page_settings_media_mobile_image', get_the_ID() ) ?: get_post_thumbnail_id( get_the_ID() ) ?: ( $attributes['wpbs-background']['mobileImage']['id'] ?? $attributes['wpbs-background']['largeImage']['id'] ?? false );
-			$img_src_mobile = wp_get_attachment_image_src( $img_id_mobile, $attributes['wpbs-background']['resolutionMobile'] ?? $attributes['wpbs-background']['resolution'] ?? 'large' )[0] ?? '#';
+				if ( $term && isset( $term->term_id ) ) {
+					$term_ref = "{$term->taxonomy}_{$term->term_id}";
+
+					$img_id_large  = get_field( 'featured_image', $term_ref );
+					$img_id_mobile = get_field( 'featured_image_mobile', $term_ref );
+				}
+			} else {
+				$img_id_large  = get_post_thumbnail_id( get_the_ID() ) ?: ( $attributes['wpbs-background']['largeImage']['id'] ?? $attributes['wpbs-background']['mobileImage']['id'] ?? false );
+				$img_id_mobile = get_field( 'page_settings_media_mobile_image', get_the_ID() ) ?: get_post_thumbnail_id( get_the_ID() ) ?: ( $attributes['wpbs-background']['mobileImage']['id'] ?? $attributes['wpbs-background']['largeImage']['id'] ?? false );
+			}
+
+			$img_src_large = wp_get_attachment_image_src( $img_id_large ?? false, $attributes['wpbs-background']['resolution'] ?? 'large' )[0] ?? '#';
+
+
+			$img_src_mobile = wp_get_attachment_image_src( $img_id_mobile ?? false, $attributes['wpbs-background']['resolutionMobile'] ?? $attributes['wpbs-background']['resolution'] ?? 'large' )[0] ?? '#';
 
 			$css = str_replace(
 				[ '%POST_IMG_URL_LARGE%', '%POST_IMG_URL_MOBILE%' ],

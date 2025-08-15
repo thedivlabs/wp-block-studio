d53e899cb0cfc673d2fc3c94c69f9af0aa9a002a
Alex Ferrao <alexsr20det@gmail.com>
2025-08-15 14:37:36 -0400

    Dev

--- app/dev/js/theme.js
+++ app/dev/js/theme.js
@@ -54,31 +54,39 @@ class WPBS_Theme {
         }
 
 
-        fetch('/wp-admin/admin-ajax.php' + '?action=team_profile&postId=6467')
+        fetch('/wp-admin/admin-ajax.php?action=team_profile&postId=6467')
             .then(res => res.json())
             .then(response => {
-
                 const {data} = response;
 
-                console.log(data);
-
-                const element = jQuery(data?.rendered).find('.wpbs-team-member-profile');
-
-                element.appendTo('body');
-
-                data.styles.forEach(url => {
+                // 1. Inject block CSS files
+                Object.values(data.styles).forEach(url => {
+                    if (!url) return;
                     const link = document.createElement('link');
                     link.rel = 'stylesheet';
                     link.href = url;
                     document.head.appendChild(link);
                 });
 
+                // 2. Inject inline CSS
                 if (data.inline_css.length) {
                     const style = document.createElement('style');
                     style.innerHTML = data.inline_css.join("\n");
                     document.head.appendChild(style);
                 }
+
+                // 3. Parse HTML string into DOM nodes
+                const template = document.createElement('div');
+                template.innerHTML = data.rendered;
+
+                // 4. Extract the template part element
+                const element = template.querySelector('.wpbs-team-member-profile');
+                if (!element) return;
+
+                // 5. Append to the body (or any container)
+                document.body.appendChild(element);
             });
+
 
     }
 

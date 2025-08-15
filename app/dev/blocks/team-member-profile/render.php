36163aea5447ff35fc4ba1c452a933d36a78d49a
Alex Ferrao <alexsr20det@gmail.com>
2025-08-15 10:20:57 -0400

    Dev

--- app/dev/blocks/team-member-profile/render.php
+++ app/dev/blocks/team-member-profile/render.php
@@ -1,2 +1,30 @@
 <?php
-echo 'PPP';
+
+$postId = $_GET['postId'] ?? $attributes['postId'] ?? $context['wpbs/postId'] ?? false;
+
+if ( empty( $postId ) ) {
+	return;
+}
+
+$fields = get_field( 'wpbs', $postId );
+
+if ( empty( $fields ) || empty( $content ) ) {
+	return;
+}
+
+$wrapper_attributes = get_block_wrapper_attributes( [
+	'class' => implode( ' ', array_filter( [
+		'wpbs-team-member-profile',
+		$attributes['uniqueId'] ?? ''
+	] ) ),
+	...( $attributes['wpbs-props'] ?? [] )
+] );
+
+?>
+
+
+<div <?= $wrapper_attributes ?>>
+
+	<?= $content ?>
+
+</div>

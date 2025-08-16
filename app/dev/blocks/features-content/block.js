36162a9b97505596c48078c4b7eba1c929452afd
Alex Ferrao <alexsr20det@gmail.com>
2025-08-16 12:16:48 -0400

    Dev

---
+++ app/dev/blocks/features-content/block.js
@@ -0,0 +1,242 @@
+import './scss/block.scss';
+
+import {
+    InspectorControls, PanelColorSettings,
+    useBlockProps,
+} from "@wordpress/block-editor"
+import {registerBlockType} from "@wordpress/blocks"
+import metadata from "./block.json"
+import {LAYOUT_ATTRIBUTES, LayoutControls} from "Components/Layout"
+import {Style, STYLE_ATTRIBUTES} from "Components/Style"
+import React, {useCallback, useMemo} from "react";
+import {useUniqueId} from "Includes/helper";
+import {DIMENSION_UNITS_TEXT, RESOLUTION_OPTIONS} from "Includes/config";
+import {
+    __experimentalGrid as Grid,
+    PanelBody,
+    TextControl,
+    __experimentalUnitControl as UnitControl,
+    __experimentalNumberControl as NumberControl,
+    SelectControl, BaseControl, ToggleControl,
+} from "@wordpress/components";
+import {useSelect} from "@wordpress/data";
+import {LinkPost} from "Components/LinkPost";
+
+function sectionClassNames(attributes = {}) {
+
+    const {'wpbs-services-content': settings = {}} = attributes;
+
+    return [
+        'wpbs-services-content',
+        'w-fit inline-block',
+        settings?.icon ? '--icon' : null,
+        attributes?.uniqueId ?? '',
+    ].filter(x => x).join(' ');
+}
+
+const CONTENT_OPTIONS = [
+    {label: 'Select', value: ''},
+    {label: 'Overview', value: 'overview'},
+    {label: 'Description', value: 'description'},
+    {label: 'Text', value: 'text'},
+    {label: 'Poster', value: 'poster'},
+    {label: 'Thumbnail', value: 'thumbnail'},
+    {label: 'Icon', value: 'icon'},
+    {label: 'Related Title', value: 'related-title'},
+    {label: 'Related Text', value: 'related-text'},
+    {label: 'CTA Title', value: 'cta-title'},
+    {label: 'CTA Text', value: 'cta-text'},
+    {label: 'CTA Image', value: 'cta-image'},
+    {label: 'FAQ Title', value: 'faq-title'},
+    {label: 'FAQ Text', value: 'faq-text'},
+
+];
+
+registerBlockType(metadata.name, {
+    apiVersion: 3,
+    attributes: {
+        ...metadata.attributes,
+        ...LAYOUT_ATTRIBUTES,
+        ...STYLE_ATTRIBUTES
+    },
+    edit: ({attributes, setAttributes, clientId}) => {
+
+        const uniqueId = useUniqueId(attributes, setAttributes, clientId);
+
+        const services = useSelect((select) => {
+            return select('core').getEntityRecords('postType', 'service', {per_page: -1});
+        }, []);
+
+        const {'wpbs-services-content': settings = {}} = attributes;
+
+        const fields = useSelect((select) => {
+
+            if (!settings?.['service-id']) {
+                return {};
+            }
+
+            const post = select('core').getEntityRecord('postType', 'service', settings?.['service-id']);
+
+            return post?.acf?.wpbs || null;
+
+        }, [settings?.['service-id']]);
+
+        const cssProps = useMemo(() => {
+            return Object.fromEntries(Object.entries({
+                '--icon': !!settings?.icon ? '"\\' + settings?.icon + '"' : null,
+                '--icon-color': settings?.['icon-color'] ?? null,
+                '--icon-size': settings?.['icon-size'] ?? null,
+                '--line-clamp': settings?.['line-clamp'] ?? null,
+            }).filter(x => x));
+        }, [settings]);
+
+        const updateSettings = useCallback((newValue) => {
+            const result = {
+                ...attributes['wpbs-services-content'],
+                ...newValue
+            };
+
+            setAttributes({
+                'wpbs-services-content': result,
+            });
+        }, [setAttributes, attributes['wpbs-services-content']]);
+
+        const blockProps = useBlockProps({
+            className: sectionClassNames(attributes),
+        });
+
+        const label = useMemo(() => {
+
+            const {type = ''} = settings;
+
+            return CONTENT_OPTIONS.find(item => item.value === type)?.label ?? 'Service Content';
+
+        }, [settings?.type]);
+
+        return (
+            <>
+
+                <InspectorControls group="styles">
+                    <PanelBody initialOpen={true} title={'Settings'}>
+                        <Grid columns={1} columnGap={15} rowGap={20}>
+
+                            <SelectControl
+                                label="Select Service"
+                                value={settings?.['service-id'] ?? ''}
+                                options={[
+                                    {label: 'Select a service', value: ''},
+                                    {label: 'Current', value: 'current'},
+                                    ...(services || []).map(post => ({
+                                        label: post.title.rendered,
+                                        value: String(post.id)
+                                    }))
+                                ]}
+                                onChange={(newValue) => updateSettings({'service-id': newValue})}
+                                __nextHasNoMarginBottom={true}
+                                __next40pxDefaultSize={true}
+                            />
+
+                            <SelectControl
+                                __nextHasNoMarginBottom={true}
+                                __next40pxDefaultSize={true}
+                                label="Type"
+                                value={settings?.type ?? ''}
+                                options={CONTENT_OPTIONS}
+                                onChange={(newValue) => updateSettings({type: newValue})}
+                            />
+
+                            <Grid columns={2} columnGap={15} rowGap={20}>
+
+                                <NumberControl
+                                    __nextHasNoMarginBottom
+                                    __next40pxDefaultSize
+                                    label="Line Clamp"
+                                    value={settings?.['line-clamp']}
+                                    onChange={(newValue) => updateSettings({'line-clamp': newValue})}
+
+                                />
+
+                                <SelectControl
+                                    __nextHasNoMarginBottom={true}
+                                    __next40pxDefaultSize={true}
+                                    label="Resolution"
+                                    value={settings?.resolution ?? ''}
+                                    options={RESOLUTION_OPTIONS}
+                                    onChange={(newValue) => updateSettings({resolution: newValue})}
+                                />
+
+                                <TextControl
+                                    __nextHasNoMarginBottom
+                                    __next40pxDefaultSize
+                                    label="Icon"
+                                    value={settings?.icon}
+                                    onChange={(newValue) => updateSettings({icon: newValue})}
+                                />
+
+                                <UnitControl
+                                    __nextHasNoMarginBottom
+                                    __next40pxDefaultSize
+                                    units={DIMENSION_UNITS_TEXT}
+                                    label="Icon Size"
+                                    value={settings?.['icon-size']}
+                                    onChange={(newValue) => updateSettings({'icon-size': newValue})}
+                                />
+
+                            </Grid>
+
+                            <TextControl
+                                __nextHasNoMarginBottom
+                                __next40pxDefaultSize
+                                label="Label"
+                                value={settings?.label}
+                                onChange={(newValue) => updateSettings({label: newValue})}
+                            />
+
+                            <BaseControl label={'Colors'}>
+                                <PanelColorSettings
+                                    enableAlpha
+                                    className={'!p-0 !border-0 [&_.components-tools-panel-item]:!m-0'}
+                                    colorSettings={[
+                                        {
+                                            slug: 'icon-color',
+                                            label: 'Icon Color',
+                                            value: settings?.['icon-color'],
+                                            onChange: (newValue) => updateSettings({'icon-color': newValue}),
+                                            isShownByDefault: true
+                                        }
+                                    ]}
+                                />
+                            </BaseControl>
+
+                            <Grid columns={2} columnGap={15} rowGap={20}>
+                                <ToggleControl
+                                    __nextHasNoMarginBottom
+                                    label="Eager"
+                                    checked={!!settings?.eager}
+                                    onChange={(newValue) => updateSettings({eager: newValue})}
+                                />
+                            </Grid>
+                        </Grid>
+                    </PanelBody>
+                </InspectorControls>
+
+                <LinkPost defaultValue={settings?.['link-post']}
+                          callback={(newValue) => updateSettings({'link-post': newValue})}/>
+
+                <LayoutControls attributes={attributes} setAttributes={setAttributes}/>
+                <Style attributes={attributes} setAttributes={setAttributes} selector={'wpbs-services-content'}
+                       uniqueId={uniqueId} props={cssProps} deps={['wpbs-services-content']}
+                />
+
+                <div {...blockProps}>
+                    {label}
+                </div>
+
+
+            </>
+        )
+    },
+    save: (props) => null
+})
+
+
---
+++ app/dev/blocks/features-content/render.php
@@ -0,0 +1,94 @@
+<?php
+
+$settings = $attributes['wpbs-services-content'] ?? false;
+
+if ( empty( $settings ) ) {
+	return;
+}
+
+$type       = $settings['type'] ?? false;
+$service_id = ( $settings['service-id'] ?? false ) == 'current' ? get_the_ID() : intval( $settings['service-id'] ?? false );
+
+if ( ! $type || ! $service_id ) {
+	return;
+}
+
+$content = match ( $type ) {
+	'overview' => get_field( 'wpbs_content_overview', $service_id ),
+	'description' => get_field( 'wpbs_content_description', $service_id ),
+	'text' => get_field( 'wpbs_content_text', $service_id ),
+	'poster' => get_field( 'wpbs_media_featured_poster', $service_id ),
+	'thumbnail' => get_field( 'wpbs_media_featured_thumbnail', $service_id ),
+	'icon' => get_field( 'wpbs_media_featured_icon', $service_id ),
+	'faq-title' => get_field( 'wpbs_faq_content_title', $service_id ),
+	'faq-text' => get_field( 'wpbs_faq_content_text', $service_id ),
+	'related-title' => get_field( 'wpbs_related_content_title', $service_id ),
+	'related-text' => get_field( 'wpbs_related_content_text', $service_id ),
+	'cta-title' => get_field( 'wpbs_cta_content_title', $service_id ),
+	'cta-text' => get_field( 'wpbs_cta_content_text', $service_id ),
+	'cta-image' => WPBS::clean_array( get_field( 'wpbs_cta_media', $service_id ) ),
+	default => null
+};
+
+if ( empty( $content ) ) {
+	return false;
+}
+
+$wrapper_attributes = get_block_wrapper_attributes( [
+	'class' => implode( ' ', array_filter( [
+		'wpbs-services-content inline-block',
+		! empty( $settings['line-clamp'] ) ? '--line-clamp' : null,
+		$attributes['uniqueId'] ?? ''
+	] ) ),
+	...( $attributes['wpbs-props'] ?? [] )
+] );
+
+$is_link = ! empty( $settings['link-post'] );
+
+$link_target = ! empty( $settings['link-post']['linkNewTab'] ) ? '_blank' : '_self';
+$link_rel    = $settings['link-post']['linkRel'] ?? '';
+$link_title  = $settings['link-post']['linkTitle'] ?? '';
+
+$loading = ! empty( $settings['eager'] ) ? 'eager' : 'lazy';
+
+?>
+
+<?php
+
+
+if ( $is_link ) {
+	echo '<a href="' . get_the_permalink( $service_id ) . '" target="' . $link_target . '" ' . $wrapper_attributes . ' title="' . $link_title . '">';
+} else {
+	echo '<div ' . $wrapper_attributes . '>';
+}
+
+switch ( $type ) {
+	case 'poster':
+	case 'thumbnail':
+		echo wp_get_attachment_image( $content, ( $settings['resolution'] ?? 'large' ), false, [
+			'loading' => $loading,
+			'class'   => 'w-full h-full object-cover'
+		] );
+		break;
+	case 'cta-image':
+
+		echo WPBS::picture(
+			$content['image_mobile'] ?? false,
+			$content['image_large'] ?? false,
+			$attributes['wpbs-breakpoint']['large'] ?? false,
+			$settings['resolution'] ?? false, $loading );
+		break;
+	default:
+		echo $content;
+}
+
+
+if ( $is_link ) {
+	echo '</a>';
+} else {
+	echo '</div>';
+}
+
+?>
+
+

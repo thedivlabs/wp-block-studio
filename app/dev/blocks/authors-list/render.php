378c82a20976ae01c702d581357bedf9220ee8b3
Alex Ferrao <alexsr20det@gmail.com>
2025-08-31 16:51:35 -0400

    Dev

--- app/dev/blocks/authors-list/render.php
+++ app/dev/blocks/authors-list/render.php
@@ -5,6 +5,8 @@ $authors = get_users( [
 	'has_published_posts' => true,
 ] );
 
+$current_author = is_author() ? get_queried_object_id() : false;
+
 if ( empty( $authors ) ) {
 	return;
 }
@@ -19,7 +21,12 @@ $wrapper_attributes = get_block_wrapper_attributes( [
 echo '<ul ' . $wrapper_attributes . '>';
 
 foreach ( $authors as $author ) {
-	echo '<li><a href="' . esc_url( get_author_posts_url( $author->ID ) ) . '">' . esc_html( $author->display_name ) . '</a></li>';
+	$is_current   = $current_author == $author->ID;
+	$item_classes = implode( ' ', array_filter( [
+		'wpbs-authors-list__item',
+		$is_current ? 'current-author' : null,
+	] ) );
+	echo '<li class="' . $item_classes . '"><a href="' . esc_url( get_author_posts_url( $author->ID ) ) . '" ' . ( $is_current ? 'aria-current="author"' : '' ) . '>' . esc_html( $author->display_name ) . '</a></li>';
 }
 
 echo '</ul>';

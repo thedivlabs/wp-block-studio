ef5a76a75947a7e98c67cdd9f5c34dba37f37f7e
Alex Ferrao <alexsr20det@gmail.com>
2025-08-31 12:40:27 -0400

    Dev

--- app/dev/blocks/archive-filters/render.php
+++ app/dev/blocks/archive-filters/render.php
@@ -1,10 +1,20 @@
 <?php
 
-$search_value = isset( $_GET['s'] ) ? esc_attr( $_GET['s'] ) : '';
-$sort_value   = isset( $_GET['sort'] ) ? esc_attr( $_GET['sort'] ) : '';
+$settings        = $block->attributes['wpbs-archive-filters'] ?? [];
+$type            = $settings['type'] ?? false;
+$search_value    = isset( $_GET['s'] ) ? esc_attr( $_GET['s'] ) : '';
+$sort_value      = isset( $_GET['sort'] ) ? esc_attr( $_GET['sort'] ) : '';
+$current_term_id = get_queried_object()->term_id ?? '';
 
 
-$search  = [ '#--SEARCH--#', '#--SORT--#' ];
-$replace = [ $search_value, $sort_value ];
+WPBS::console_log( get_queried_object() );
+WPBS::console_log( $current_term_id );
+
+$search  = [ '#--SEARCH--#', '#--SORT--#', '#--TERMS--#' ];
+$replace = [ $search_value, $sort_value, $current_term_id ];
+
+if ( $type == 'terms' ) {
+	$search[] = '</select>';
+}
 
 echo str_replace( $search, $replace, $content ?? '' );

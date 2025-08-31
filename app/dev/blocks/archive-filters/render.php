03757d48ecdf219a8dac53dd79f921f3c9f35289
Alex Ferrao <alexsr20det@gmail.com>
2025-08-31 15:30:59 -0400

    Dev

--- app/dev/blocks/archive-filters/render.php
+++ app/dev/blocks/archive-filters/render.php
@@ -1,9 +1,5 @@
 <?php
 
-global $wp_query;
-
-WPBS::console_log( $wp_query );
-
 $settings        = $block->attributes['wpbs-archive-filters'] ?? [];
 $type            = $settings['type'] ?? false;
 $search_value    = isset( $_GET['s'] ) ? esc_attr( $_GET['s'] ) : '';
@@ -11,12 +7,9 @@ $sort_value      = isset( $_GET['sort'] ) ? esc_attr( $_GET['sort'] ) : '';
 $current_term_id = get_queried_object()->term_id ?? '';
 
 $post_type          = get_post_type_object( ( get_query_var( 'post_type' ) ?: 'post' ) );
-$current_taxonomies = array_filter( get_object_taxonomies( $post_type->name ?? false, 'objects' ), function ( $tax ) {
+$current_taxonomies = array_filter( ! empty( $settings['taxonomy'] ) ? [ get_taxonomy( $settings['taxonomy'] ) ] : get_object_taxonomies( $post_type->name ?? false, 'objects' ), function ( $tax ) {
 	return is_a( $tax, 'WP_Taxonomy' ) && $tax->public && $tax->hierarchical;
 } );
-
-WPBS::console_log( $current_term_id );
-WPBS::console_log( $current_taxonomies );
 
 $search  = [ '#--SEARCH--#', '#--SORT--#', '#--TERMS--#' ];
 $replace = [ $search_value, $sort_value, $current_term_id ];

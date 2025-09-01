cad228252eee679197e493fc9749b96b8bf2f9ae
Alex Ferrao <alexsr20det@gmail.com>
2025-09-01 12:38:58 -0400

    Dev

--- app/dev/blocks/odometer/view.js
+++ app/dev/blocks/odometer/view.js
@@ -1,38 +1,42 @@
 import {getContext, getElement, store} from '@wordpress/interactivity';
 
+function waitForOdometer(cb) {
+    if (typeof Odometer !== 'undefined') {
+        cb();
+    } else {
+        setTimeout(() => waitForOdometer(cb), 50);
+    }
+}
+
 const {state} = store('wpbs/odometer', {
     actions: {
         init: () => {
-            const {ref: element} = getElement();
-            const context = getContext();
-            const {start = 0, end = 100, duration = 1200, format = false} = context;
-            const link_ID = 'wpbs-odometer-css';
-            const script_ID = 'wpbs-odometer-js';
-
-            if (typeof Odometer !== 'undefined') {
-                const od = new Odometer({
-                    el: element,
-                    value: start,
-                    duration: duration,
-                    theme: 'default',
-                    format: format ? '(,ddd).dd' : '',
-                    framesPerSecond: 60,
-                });
+            waitForOdometer(() => {
+                const {ref: element} = getElement();
+                const context = getContext();
+                const {start = 0, end = 100, duration = 1200, format = false} = context;
 
                 const observer = new IntersectionObserver((entries) => {
                     entries.forEach(entry => {
                         if (entry.isIntersecting) {
-                            // Animate to final value when in view
+                            const od = new Odometer({
+                                el: element,
+                                value: start,
+                                duration: duration,
+                                theme: 'default',
+                                format: format ? '(,ddd).dd' : '',
+                                framesPerSecond: 60,
+                            });
                             od.update(end);
                         } else {
                             // Reset back to start when out of view
-                            od.update(start);
+                            //od.update(start);
                         }
                     });
                 }, {threshold: 0.3});
 
                 observer.observe(element);
-            }
+            });
         },
     },
 });

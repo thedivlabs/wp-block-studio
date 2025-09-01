786110d46140d3d34417fd894616f5559139ad99
Alex Ferrao <alexsr20det@gmail.com>
2025-09-01 12:53:40 -0400

    Dev

--- app/dev/js/modules/reveal.js
+++ app/dev/js/modules/reveal.js
@@ -5,29 +5,30 @@ export default class Reveal {
         if (!document.querySelector('[data-aos]')) {
             return false;
         }
+        wp.domReady(() => {
 
-        // Inject AOS CSS
-        const link = document.createElement('link');
-        link.href = 'https://cdn.jsdelivr.net/npm/aos/dist/aos.min.css';
-        link.rel = 'stylesheet';
-        link.type = 'text/css';
-        document.head.appendChild(link);
+            // Inject AOS CSS
+            const link = document.createElement('link');
+            link.href = 'https://cdn.jsdelivr.net/npm/aos/dist/aos.min.css';
+            link.rel = 'stylesheet';
+            link.type = 'text/css';
+            document.head.appendChild(link);
 
-        // Load AOS JS dynamically
-        const script = document.createElement('script');
-        script.src = 'https://cdn.jsdelivr.net/npm/aos/dist/aos.min.js';
-        script.onload = () => {
-            if (typeof AOS !== 'undefined') {
-                AOS.init({
-                    disable: function () {
-                        const maxWidth = parseInt(WPBS?.settings?.breakpoints?.normal ?? '768');
-                        return window.innerWidth < maxWidth;
-                    }
-                });
-            }
-        };
-        document.body.appendChild(script);
+            // Load AOS JS dynamically
+            const script = document.createElement('script');
+            script.src = 'https://cdn.jsdelivr.net/npm/aos/dist/aos.min.js';
+            script.onload = () => {
+                if (typeof AOS !== 'undefined') {
+                    AOS.init({
+                        disable: function () {
+                            const maxWidth = parseInt(WPBS?.settings?.breakpoints?.normal ?? '768');
+                            return window.innerWidth < maxWidth;
+                        }
+                    });
+                }
+            };
+            document.body.appendChild(script);
+        })
 
-        return true;
     }
 }

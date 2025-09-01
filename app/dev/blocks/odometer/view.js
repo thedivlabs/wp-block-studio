a45712aa9a8b47a54b683c36ed07b4ee490280f6
Alex Ferrao <alexsr20det@gmail.com>
2025-09-01 12:44:02 -0400

    Dev

--- app/dev/blocks/odometer/view.js
+++ app/dev/blocks/odometer/view.js
@@ -1,12 +1,5 @@
 import {getContext, getElement, store} from '@wordpress/interactivity';
 
-function waitForOdometer(cb) {
-    if (typeof Odometer !== 'undefined') {
-        cb();
-    } else {
-        setTimeout(() => waitForOdometer(cb), 50);
-    }
-}
 
 const {state} = store('wpbs/odometer', {
     actions: {
@@ -16,24 +9,33 @@ const {state} = store('wpbs/odometer', {
             const context = getContext();
             const {start = 0, end = 100, duration = 1200, format = false} = context;
 
+            let od;
+
+            function waitForOdometer(cb) {
+                if (typeof Odometer !== 'undefined') {
+                    od = new Odometer({
+                        el: element,
+                        value: start,
+                        duration: duration,
+                        theme: 'default',
+                        format: format ? '(,ddd).dd' : '',
+                        framesPerSecond: 60,
+                    });
+                    cb();
+                } else {
+                    setTimeout(() => waitForOdometer(cb), 100);
+                }
+            }
+
             waitForOdometer(() => {
 
 
                 const observer = new IntersectionObserver((entries) => {
                     entries.forEach(entry => {
                         if (entry.isIntersecting) {
-                            const od = new Odometer({
-                                el: element,
-                                value: start,
-                                duration: duration,
-                                theme: 'default',
-                                format: format ? '(,ddd).dd' : '',
-                                framesPerSecond: 60,
-                            });
                             od.update(end);
                         } else {
-                            // Reset back to start when out of view
-                            //od.update(start);
+                            od.update(start);
                         }
                     });
                 }, {threshold: 0.3});

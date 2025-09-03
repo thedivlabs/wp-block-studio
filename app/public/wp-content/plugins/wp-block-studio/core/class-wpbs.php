a990464a3ffb893d6b09802e6a14b8e8f3c56f8f
thedivlabs <121818117+thedivlabs@users.noreply.github.com>
2025-09-03 14:32:29 -0400

    Dev

--- app/public/wp-content/plugins/wp-block-studio/core/class-wpbs.php
+++ app/public/wp-content/plugins/wp-block-studio/core/class-wpbs.php
@@ -199,6 +199,8 @@ class WPBS {
 		wp_enqueue_script( 'wpbs-theme-js' );
 		wp_enqueue_style( 'google-material-icons' );
 		//wp_enqueue_script( 'wpbs-admin-js' );
+		wp_enqueue_script( 'wpbs-swiper-js' );
+		wp_enqueue_style( 'wpbs-swiper-css' );
 
 	}
 

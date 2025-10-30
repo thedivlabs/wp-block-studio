da1c955625a44295ea1c29c8fa5589bbce4298d8
Alex Ferrao <aferrao@divlabs.dev>
2025-10-30 14:19:21 -0400

    Dev

--- app/dev/js/components/Field.js
+++ app/dev/js/components/Field.js
@@ -183,6 +183,7 @@ export const Field = memo(({field, settings, callback}) => {
                             onBlur={handleBlur}
                             aria-label={label}
                             onKeyDown={handleKeyDown}
+                            isResetValueOnUnitChange
                             {...controlProps}
                         />
                     </div>

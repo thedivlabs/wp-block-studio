3d31d4e6ee0c8536ab9ed91f9947c5454d663141
Alex Ferrao <aferrao@divlabs.dev>
2025-11-05 15:15:29 -0500

    Dev

--- app/dev/js/components/Style.js
+++ app/dev/js/components/Style.js
@@ -1,11 +1,27 @@
 import {Fragment, useCallback, useEffect, useMemo, useRef, useState} from '@wordpress/element';
-import {InnerBlocks, InspectorControls, useBlockProps, useInnerBlocksProps} from '@wordpress/block-editor';
+import {
+    InnerBlocks,
+    InspectorControls, MediaUpload,
+    MediaUploadCheck, PanelColorSettings,
+    useBlockProps,
+    useInnerBlocksProps
+} from '@wordpress/block-editor';
 import {ElementTagControl, getElementTag} from "Components/ElementTag";
 import {StyleEditorUI} from "Includes/style";
 import {isEqual} from 'lodash';
-import {__experimentalGrid as Grid, ToggleControl,} from "@wordpress/components";
+import {
+    __experimentalUnitControl as UnitControl,
+    __experimentalGrid as Grid,
+    BaseControl,
+    Button, GradientPicker,
+    PanelBody, RangeControl,
+    SelectControl,
+    ToggleControl,
+} from "@wordpress/components";
 import {useInstanceId} from "@wordpress/compose";
 import {select, subscribe} from "@wordpress/data";
+import React from "react";
+import {BLEND_OPTIONS, IMAGE_SIZE_OPTIONS, ORIGIN_OPTIONS, POSITION_OPTIONS, RESOLUTION_OPTIONS} from "Includes/config";
 
 export const STYLE_ATTRIBUTES = {
     'uniqueId': {
@@ -171,6 +187,340 @@ const BlockWrapper = ({
     );
 };
 
+const BackgroundControls = ({settings, callback}) => {
+    return <PanelBody title="Background" initialOpen={!!settings.type}>
+        <Grid columns={1} columnGap={15} rowGap={20}>
+            <SelectControl
+                __next40pxDefaultSize
+                label="Type"
+                value={settings?.type}
+                onChange={(newValue) => callback({type: newValue})}
+                options={[
+                    {label: 'Select', value: ''},
+                    {label: 'Image', value: 'image'},
+                    {label: 'Featured Image', value: 'featured-image'},
+                    {label: 'Video', value: 'video'},
+                ]}
+                __nextHasNoMarginBottom
+            />
+
+            {/* Only show once a type is selected */}
+            {settings.type && (
+                <Grid columns={1} columnGap={15} rowGap={20}>
+                    {/* IMAGE / FEATURED IMAGE */}
+                    {(settings.type === 'image' || settings.type === 'featured-image') && (
+                        <Grid columns={2} columnGap={15} rowGap={20}>
+                            <MediaUploadCheck>
+                                <MediaUpload
+                                    onSelect={(newValue) =>
+                                        callback({
+                                            mobileImage: newValue,
+                                        })
+                                    }
+                                    onClose={() => {
+                                    }}
+                                    allowedTypes={['image']}
+                                    render={({open}) => (
+                                        <Button
+                                            onClick={open}
+                                            variant="secondary"
+                                            className="!w-full"
+                                        >
+                                            {settings.mobileImage?.url
+                                                ? 'Change Mobile Image'
+                                                : 'Select Mobile Image'}
+                                        </Button>
+                                    )}
+                                />
+                            </MediaUploadCheck>
+
+                            <MediaUploadCheck>
+                                <MediaUpload
+                                    onSelect={(newValue) =>
+                                        callback({
+                                            largeImage: newValue,
+                                        })
+                                    }
+                                    allowedTypes={['image']}
+                                    render={({open}) => (
+                                        <Button
+                                            onClick={open}
+                                            variant="secondary"
+                                            className="!w-full"
+                                        >
+                                            {settings.largeImage?.url
+                                                ? 'Change Large Image'
+                                                : 'Select Large Image'}
+                                        </Button>
+                                    )}
+                                />
+                            </MediaUploadCheck>
+                        </Grid>
+                    )}
+
+                    {/* VIDEO */}
+                    {settings.type === 'video' && (
+                        <Grid columns={2} columnGap={15} rowGap={20}>
+                            <MediaUploadCheck>
+                                <MediaUpload
+                                    onSelect={(newValue) =>
+                                        callback({
+                                            mobileVideo: newValue,
+                                        })
+                                    }
+                                    allowedTypes={['video']}
+                                    render={({open}) => (
+                                        <Button
+                                            onClick={open}
+                                            variant="secondary"
+                                            className="!w-full"
+                                        >
+                                            {settings.mobileVideo?.url
+                                                ? 'Change Mobile Video'
+                                                : 'Select Mobile Video'}
+                                        </Button>
+                                    )}
+                                />
+                            </MediaUploadCheck>
+
+                            <MediaUploadCheck>
+                                <MediaUpload
+                                    onSelect={(newValue) =>
+                                        callback({
+                                            largeVideo: newValue,
+                                        })
+                                    }
+                                    allowedTypes={['video']}
+                                    render={({open}) => (
+                                        <Button
+                                            onClick={open}
+                                            variant="secondary"
+                                            className="!w-full"
+                                        >
+                                            {settings.largeVideo?.url
+                                                ? 'Change Large Video'
+                                                : 'Select Large Video'}
+                                        </Button>
+                                    )}
+                                />
+                            </MediaUploadCheck>
+                        </Grid>
+                    )}
+
+                    {/* TOGGLES */}
+                    <Grid columns={3} columnGap={15} rowGap={20} style={{padding: '1rem 0'}}>
+                        <ToggleControl
+                            label="Eager"
+                            checked={!!settings?.eager}
+                            onChange={(newValue) => callback({eager: newValue})}
+                        />
+                        <ToggleControl
+                            label="Force"
+                            checked={!!settings?.force}
+                            onChange={(newValue) => callback({force: newValue})}
+                        />
+                        <ToggleControl
+                            label="Fixed"
+                            checked={!!settings?.fixed}
+                            onChange={(newValue) => callback({fixed: newValue})}
+                        />
+                    </Grid>
+
+                    <Grid columns={1} columnGap={15} rowGap={20}>
+                        <Grid columns={2} columnGap={15} rowGap={20}>
+                            <SelectControl
+                                __next40pxDefaultSize
+                                label="Resolution"
+                                value={settings?.resolution}
+                                onChange={(newValue) => callback({resolution: newValue})}
+                                options={RESOLUTION_OPTIONS}
+                                __nextHasNoMarginBottom
+                            />
+                            <SelectControl
+                                __next40pxDefaultSize
+                                label="Size"
+                                value={settings?.size}
+                                onChange={(newValue) => callback({size: newValue})}
+                                options={IMAGE_SIZE_OPTIONS}
+                                __nextHasNoMarginBottom
+                            />
+                            <SelectControl
+                                __next40pxDefaultSize
+                                label="Blend"
+                                value={settings?.blend}
+                                onChange={(newValue) => callback({blend: newValue})}
+                                options={BLEND_OPTIONS}
+                                __nextHasNoMarginBottom
+                            />
+                            <SelectControl
+                                __next40pxDefaultSize
+                                label="Position"
+                                value={settings?.position}
+                                onChange={(newValue) => callback({position: newValue})}
+                                options={POSITION_OPTIONS}
+                                __nextHasNoMarginBottom
+                            />
+                            <SelectControl
+                                __next40pxDefaultSize
+                                label="Origin"
+                                value={settings?.origin}
+                                onChange={(newValue) => callback({origin: newValue})}
+                                options={ORIGIN_OPTIONS}
+                                __nextHasNoMarginBottom
+                            />
+                            <UnitControl
+                                label="Max Height"
+                                value={settings?.maxHeight}
+                                onChange={(newValue) => callback({maxHeight: newValue})}
+                                units={[{value: 'vh', label: 'vh', default: 0}]}
+                            />
+                            <SelectControl
+                                __next40pxDefaultSize
+                                label="Repeat"
+                                value={settings?.repeat}
+                                onChange={(newValue) => callback({repeat: newValue})}
+                                options={REPEAT_OPTIONS}
+                                __nextHasNoMarginBottom
+                            />
+                        </Grid>
+
+                        <Grid columns={1} columnGap={15} rowGap={20}>
+                            <PanelColorSettings
+                                enableAlpha
+                                className="!p-0 !border-0 [&_.components-tools-panel-item]:!m-0"
+                                colorSettings={[
+                                    {
+                                        slug: 'color',
+                                        label: 'Color',
+                                        value: settings?.color ?? '',
+                                        onChange: (newValue) => callback({color: newValue}),
+                                        isShownByDefault: true,
+                                    },
+                                ]}
+                            />
+                            <RangeControl
+                                label="Scale"
+                                value={settings?.scale}
+                                onChange={(newValue) => callback({scale: newValue})}
+                                min={0}
+                                max={200}
+                            />
+                            <RangeControl
+                                label="Opacity"
+                                value={settings?.opacity}
+                                onChange={(newValue) => callback({opacity: newValue})}
+                                min={0}
+                                max={100}
+                            />
+                            <RangeControl
+                                label="Width"
+                                value={settings?.width}
+                                onChange={(newValue) => callback({width: newValue})}
+                                min={0}
+                                max={100}
+                            />
+                            <RangeControl
+                                label="Height"
+                                value={settings?.height}
+                                onChange={(newValue) => callback({height: newValue})}
+                                min={0}
+                                max={100}
+                            />
+                            <RangeControl
+                                label="Fade"
+                                value={settings?.fade}
+                                onChange={(newValue) => callback({fade: newValue})}
+                                min={0}
+                                max={100}
+                            />
+                        </Grid>
+
+                        <Grid columns={2} columnGap={15} rowGap={20} style={{padding: '1rem 0'}}>
+                            <ToggleControl
+                                label="Mask"
+                                checked={!!settings?.mask}
+                                onChange={(newValue) => callback({mask: newValue})}
+                            />
+                        </Grid>
+
+                        {settings.mask && (
+                            <Grid columns={1} columnGap={15} rowGap={20}>
+                                <MediaUploadCheck>
+                                    <MediaUpload
+                                        onSelect={(newValue) =>
+                                            callback({
+                                                maskImageLarge: {
+                                                    type: newValue.type,
+                                                    id: newValue.id,
+                                                    url: newValue.url,
+                                                    alt: newValue.alt,
+                                                    sizes: newValue.sizes,
+                                                },
+                                            })
+                                        }
+                                        allowedTypes={['image']}
+                                        render={({open}) => (
+                                            <Button onClick={open} variant="secondary">
+                                                {settings.maskImageLarge?.url
+                                                    ? 'Change Mask Image'
+                                                    : 'Select Mask Image'}
+                                            </Button>
+                                        )}
+                                    />
+                                </MediaUploadCheck>
+
+                                <Grid columns={2} columnGap={15} rowGap={20}>
+                                    <SelectControl
+                                        __next40pxDefaultSize
+                                        label="Mask Origin"
+                                        value={settings?.maskOrigin}
+                                        onChange={(newValue) => callback({maskOrigin: newValue})}
+                                        options={ORIGIN_OPTIONS}
+                                        __nextHasNoMarginBottom
+                                    />
+                                    <SelectControl
+                                        __next40pxDefaultSize
+                                        label="Mask Size"
+                                        value={settings?.maskSize}
+                                        onChange={(newValue) => callback({maskSize: newValue})}
+                                        options={IMAGE_SIZE_OPTIONS}
+                                        __nextHasNoMarginBottom
+                                    />
+                                </Grid>
+                            </Grid>
+                        )}
+
+                        <BaseControl label="Overlay" __nextHasNoMarginBottom={true}>
+                            <GradientPicker
+                                gradients={[
+                                    {
+                                        name: 'Transparent',
+                                        gradient: 'linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0))',
+                                        slug: 'transparent',
+                                    },
+                                    {
+                                        name: 'Light',
+                                        gradient: 'linear-gradient(rgba(0,0,0,.3),rgba(0,0,0,.3))',
+                                        slug: 'light',
+                                    },
+                                    {
+                                        name: 'Strong',
+                                        gradient: 'linear-gradient(rgba(0,0,0,.7),rgba(0,0,0,.7))',
+                                        slug: 'strong',
+                                    },
+                                ]}
+                                clearable
+                                value={settings?.overlay ?? undefined}
+                                onChange={(newValue) => callback({overlay: newValue})}
+                            />
+                        </BaseControl>
+                    </Grid>
+                </Grid>
+            )}
+        </Grid>
+    </PanelBody>
+}
+
 const AdvancedControls = ({settings, callback}) => {
     return <Grid columns={1} columnGap={15} rowGap={20} style={{padding: '15px 0'}}>
         <Grid columns={2} columnGap={15} rowGap={20}>
@@ -212,6 +562,7 @@ const AdvancedControls = ({settings, callback}) => {
         </Grid>
     </Grid>
 }
+
 
 export const withStyle = (Component) => (props) => {
 
@@ -259,7 +610,7 @@ export const withStyle = (Component) => (props) => {
             isEqual(cleanedLocal, cleanObject(settings, true)) &&
             isEqual(cleanedCss, cleanObject(cssObj, true))
         ) {
-           return;
+            return;
         }
 
         setAttributes({

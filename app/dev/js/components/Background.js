213ae37964737784659c262191a01bdc999164b6
Alex Ferrao <aferrao@divlabs.dev>
2025-11-09 10:35:10 -0500

    Dev

--- app/dev/js/components/Background.js
+++ app/dev/js/components/Background.js
@@ -58,9 +58,7 @@ const BackgroundFields = memo(({settings, updateFn}) => {
 
 export const BackgroundControls = ({settings = {}, callback}) => {
     const isPanelOpen = Object.keys(settings).length > 0;
-    if (!isPanelOpen) {
-        return null;
-    }
+
     console.log(isPanelOpen);
     return (
         <PanelBody title="Background" initialOpen={isPanelOpen}>
@@ -78,126 +76,129 @@ export const BackgroundControls = ({settings = {}, callback}) => {
                         {label: 'Video', value: 'video'},
                     ]}
                 />
+                {!isPanelOpen ? null : <>
+                    <div>
+                        {/* --- Media pickers under Type --- */}
+                        {(settings.type === 'image' || settings.type === 'featured-image') && (
+                            <BaseControl label="Image" __nextHasNoMarginBottom>
+                                <MediaUploadCheck>
+                                    <MediaUpload
+                                        title="Select Image"
+                                        allowedTypes={['image']}
+                                        value={settings?.image?.id}
+                                        onSelect={(media) =>
+                                            callback({
+                                                image: {
+                                                    id: media.id,
+                                                    url: media.url,
+                                                    alt: media?.alt,
+                                                    type: media?.type,
+                                                    sizes: media?.sizes,
+                                                },
+                                            })
+                                        }
+                                        render={({open}) => (
+                                            <PreviewThumbnail
+                                                image={settings?.image}
+                                                onClick={open}
+                                                callback={() => callback({image: {}})}
+                                                style={{
+                                                    objectFit: 'contain',
+                                                    borderRadius: '6px',
+                                                }}
+                                            />
+                                        )}
+                                    />
+                                </MediaUploadCheck>
+                            </BaseControl>
+                        )}
 
-                <div style={{display: !settings?.type ? 'none' : null}}>
-                    {/* --- Media pickers under Type --- */}
-                    {(settings.type === 'image' || settings.type === 'featured-image') && (
-                        <BaseControl label="Image" __nextHasNoMarginBottom>
-                            <MediaUploadCheck>
-                                <MediaUpload
-                                    title="Select Image"
-                                    allowedTypes={['image']}
-                                    value={settings?.image?.id}
-                                    onSelect={(media) =>
-                                        callback({
-                                            image: {
-                                                id: media.id,
-                                                url: media.url,
-                                                alt: media?.alt,
-                                                type: media?.type,
-                                                sizes: media?.sizes,
-                                            },
-                                        })
-                                    }
-                                    render={({open}) => (
-                                        <PreviewThumbnail
-                                            image={settings?.image}
-                                            onClick={open}
-                                            callback={() => callback({image: {}})}
-                                            style={{
-                                                objectFit: 'contain',
-                                                borderRadius: '6px',
-                                            }}
-                                        />
-                                    )}
-                                />
-                            </MediaUploadCheck>
-                        </BaseControl>
-                    )}
-
-                    {settings.type === 'video' && (
-                        <BaseControl label="Video" __nextHasNoMarginBottom>
-                            <MediaUploadCheck>
-                                <MediaUpload
-                                    title="Select Video"
-                                    allowedTypes={['video']}
-                                    value={settings?.video?.id}
-                                    onSelect={(media) =>
-                                        callback({
-                                            video: {
-                                                id: media.id,
-                                                url: media.url,
-                                                type: media?.type,
-                                                mime: media?.mime,
-                                            },
-                                        })
-                                    }
-                                    render={({open}) => (
-                                        <PreviewThumbnail
-                                            image={settings?.video}
-                                            onClick={open}
-                                            callback={() => callback({video: {}})}
-                                            style={{
-                                                objectFit: 'contain',
-                                                borderRadius: '6px',
-                                            }}
-                                        />
-                                    )}
-                                />
-                            </MediaUploadCheck>
-                        </BaseControl>
-                    )}
-                </div>
-
-                <Grid columns={2} columnGap={15} rowGap={20} style={{display: !settings?.type ? 'none' : null}}>
-                    <ToggleControl
-                        label="Eager"
-                        checked={!!settings?.['eager']}
-                        onChange={(v) => callback({eager: v})}
-                    />
-                    <ToggleControl
-                        label="Fixed"
-                        checked={!!settings?.['fixed']}
-                        onChange={(v) => callback({fixed: v})}
-                    />
-                </Grid>
-                <BaseControl label="Overlay" __nextHasNoMarginBottom={true}>
-                    <div style={{padding: '12px', backgroundColor: '#efefef', borderRadius: '6px'}}>
-                        <GradientPicker
-                            gradients={[
-                                {
-                                    name: 'Transparent',
-                                    gradient: 'linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0))',
-                                    slug: 'transparent',
-                                },
-                                {
-                                    name: 'Light',
-                                    gradient: 'linear-gradient(rgba(0,0,0,.3),rgba(0,0,0,.3))',
-                                    slug: 'light',
-                                },
-                                {
-                                    name: 'Strong',
-                                    gradient: 'linear-gradient(rgba(0,0,0,.7),rgba(0,0,0,.7))',
-                                    slug: 'strong',
-                                },
-                            ]}
-                            clearable={false}
-                            value={settings?.['overlay'] ?? undefined}
-                            onChange={(newValue) => callback({overlay: newValue})}
-                        />
+                        {settings.type === 'video' && (
+                            <BaseControl label="Video" __nextHasNoMarginBottom>
+                                <MediaUploadCheck>
+                                    <MediaUpload
+                                        title="Select Video"
+                                        allowedTypes={['video']}
+                                        value={settings?.video?.id}
+                                        onSelect={(media) =>
+                                            callback({
+                                                video: {
+                                                    id: media.id,
+                                                    url: media.url,
+                                                    type: media?.type,
+                                                    mime: media?.mime,
+                                                },
+                                            })
+                                        }
+                                        render={({open}) => (
+                                            <PreviewThumbnail
+                                                image={settings?.video}
+                                                onClick={open}
+                                                callback={() => callback({video: {}})}
+                                                style={{
+                                                    objectFit: 'contain',
+                                                    borderRadius: '6px',
+                                                }}
+                                            />
+                                        )}
+                                    />
+                                </MediaUploadCheck>
+                            </BaseControl>
+                        )}
                     </div>
-                </BaseControl>
-                <div style={{display: !settings?.type ? 'none' : null}}>
-                    <ToolsPanel
-                        label="Advanced Background"
-                        resetAll={() => callback({})}
-                    >
-                        <BackgroundFields
-                            settings={settings}
-                            updateFn={(newProps) => callback(newProps)}
+
+                    <Grid columns={2} columnGap={15} rowGap={20}>
+                        <ToggleControl
+                            label="Eager"
+                            checked={!!settings?.['eager']}
+                            onChange={(v) => callback({eager: v})}
                         />
-                    </ToolsPanel>
-                </div>
+                        <ToggleControl
+                            label="Fixed"
+                            checked={!!settings?.['fixed']}
+                            onChange={(v) => callback({fixed: v})}
+                        />
+                    </Grid>
+                    <BaseControl label="Overlay" __nextHasNoMarginBottom={true}>
+                        <div style={{padding: '12px', backgroundColor: '#efefef', borderRadius: '6px'}}>
+                            <GradientPicker
+                                gradients={[
+                                    {
+                                        name: 'Transparent',
+                                        gradient: 'linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0))',
+                                        slug: 'transparent',
+                                    },
+                                    {
+                                        name: 'Light',
+                                        gradient: 'linear-gradient(rgba(0,0,0,.3),rgba(0,0,0,.3))',
+                                        slug: 'light',
+                                    },
+                                    {
+                                        name: 'Strong',
+                                        gradient: 'linear-gradient(rgba(0,0,0,.7),rgba(0,0,0,.7))',
+                                        slug: 'strong',
+                                    },
+                                ]}
+                                clearable={false}
+                                value={settings?.['overlay'] ?? undefined}
+                                onChange={(newValue) => callback({overlay: newValue})}
+                            />
+                        </div>
+                    </BaseControl>
+                    <div>
+                        <ToolsPanel
+                            label="Advanced Background"
+                            resetAll={() => callback({})}
+                        >
+                            <BackgroundFields
+                                settings={settings}
+                                updateFn={(newProps) => callback(newProps)}
+                            />
+                        </ToolsPanel>
+                    </div>
+                </>}
+
+
             </Grid>
         </PanelBody>
 

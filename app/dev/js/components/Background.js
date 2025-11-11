be0642d8f549f07c2fa07974d4e36c9e25e1b2df
Alex Ferrao <aferrao@divlabs.dev>
2025-11-11 09:31:51 -0500

    changing UI

--- app/dev/js/components/Background.js
+++ app/dev/js/components/Background.js
@@ -117,29 +117,31 @@ export const BackgroundControls = ({settings = {}, callback, isBreakpoint = fals
                         </BaseControl>
                     )}
 
-                    <BaseControl label={'Overlay'} className={'wpbs-background-controls__card'}>
-                        <GradientPicker
-                            gradients={[
-                                {
-                                    name: 'Transparent',
-                                    gradient: 'linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0))',
-                                    slug: 'transparent',
-                                },
-                                {
-                                    name: 'Light',
-                                    gradient: 'linear-gradient(rgba(0,0,0,.3),rgba(0,0,0,.3))',
-                                    slug: 'light',
-                                },
-                                {
-                                    name: 'Strong',
-                                    gradient: 'linear-gradient(rgba(0,0,0,.7),rgba(0,0,0,.7))',
-                                    slug: 'strong',
-                                },
-                            ]}
-                            clearable={false}
-                            value={settings?.['overlay'] ?? undefined}
-                            onChange={(newValue) => callback({overlay: newValue})}
-                        />
+                    <BaseControl label={'Overlay'}>
+                        <div className={'wpbs-background-controls__card'}>
+                            <GradientPicker
+                                gradients={[
+                                    {
+                                        name: 'Transparent',
+                                        gradient: 'linear-gradient(rgba(0,0,0,0),rgba(0,0,0,0))',
+                                        slug: 'transparent',
+                                    },
+                                    {
+                                        name: 'Light',
+                                        gradient: 'linear-gradient(rgba(0,0,0,.3),rgba(0,0,0,.3))',
+                                        slug: 'light',
+                                    },
+                                    {
+                                        name: 'Strong',
+                                        gradient: 'linear-gradient(rgba(0,0,0,.7),rgba(0,0,0,.7))',
+                                        slug: 'strong',
+                                    },
+                                ]}
+                                clearable={false}
+                                value={settings?.['overlay'] ?? undefined}
+                                onChange={(newValue) => callback({overlay: newValue})}
+                            />
+                        </div>
                     </BaseControl>
 
                     {!isBreakpoint && <Grid columns={2} columnGap={15} rowGap={20}>

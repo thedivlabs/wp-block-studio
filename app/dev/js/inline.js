e5f3e43cdcfb4fee2a0c1bfd76e3213eb6573973
Alex Ferrao <aferrao@divlabs.dev>
2025-11-18 23:41:35 -0500

    Dev

--- app/dev/js/inline.js
+++ app/dev/js/inline.js
@@ -1,3 +1,10 @@
 import MediaWatcher from './modules/MediaWatcher';
 
 MediaWatcher.init();
+
+document.fonts.load('24px "Material Symbols Outlined"').then(() => {
+
+    document.querySelectorAll('.material-symbols-outlined').forEach(el => {
+        document.body.classList.add('icons-loaded');
+    })
+});

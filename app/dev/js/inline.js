4e11151c56abd01f7fd54319eb9e2ed6e68f8ee4
Alex Ferrao <aferrao@divlabs.dev>
2025-11-19 11:17:04 -0500

    Dev

--- app/dev/js/inline.js
+++ app/dev/js/inline.js
@@ -1,8 +1,7 @@
 import MediaWatcher from './modules/MediaWatcher';
 
-if (!document.body.classList.contains('wp-admin')) {
-    MediaWatcher.init();
-}
+
+MediaWatcher.init();
 
 
 document.fonts.load('24px "Material Symbols Outlined"').then(() => {

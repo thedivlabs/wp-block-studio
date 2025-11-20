5752f646f967c4be0b1683a93f92c1c35aa5b136
thedivlabs <121818117+thedivlabs@users.noreply.github.com>
2025-11-20 14:14:27 -0500

    Dev

--- app/dev/js/components/Link.js
+++ app/dev/js/components/Link.js
@@ -1,51 +1,123 @@
 import {
-    DropdownMenu, MenuGroup, MenuItem, ToolbarGroup
+    DropdownMenu,
+    MenuGroup,
+    MenuItem,
+    ToolbarGroup,
+    PanelBody,
+    TextControl,
+    ToggleControl,
+    __experimentalGrid as Grid
 } from "@wordpress/components";
+
+
 import {useState} from "@wordpress/element";
-import {__experimentalLinkControl as LinkControl, BlockControls} from "@wordpress/block-editor";
+import {
+    __experimentalLinkControl as LinkControl,
+    BlockControls
+} from "@wordpress/block-editor";
+
 import {customLink} from "@wordpress/icons";
 
-function Link({defaultValue, callback}) {
 
-    const [settings, setSettings] = useState(defaultValue || {});
+export default function Link({defaultValue = {}, callback}) {
 
-    function updateSettings(newValue) {
+    // unified link state
+    const [link, setLink] = useState({
+        url: defaultValue.url || "",
+        linkNewTab: defaultValue.linkNewTab || false,
+        title: defaultValue.title || "",
+        ariaLabel: defaultValue.ariaLabel || "",
+        id: defaultValue.id || "",
+        rel: defaultValue.rel || "",
+        // add any custom keys here
+    });
 
-        setSettings(newValue);
-        callback(newValue);
+
+    // one update fn for everything
+    function update(next) {
+        const merged = {...link, ...next};
+        setLink(merged);
+        callback?.(merged);
     }
 
 
-    return <BlockControls>
-        <ToolbarGroup>
-            <DropdownMenu
-                icon={customLink}
-                label={'Link'}
-            >
-                {({onClose}) => (
-                    <MenuGroup>
-                        <MenuItem>
-                            <LinkControl
-                                searchInputPlaceholder={"Search here..."}
-                                allowDirectEntry={true}
-                                forceIsEditingLink={true}
-                                hasTextControl={true}
-                                value={settings}
-                                settings={[
-                                    {
-                                        id: 'linkNewTab',
-                                        title: 'Open in new tab',
-                                    }
-                                ]}
-                                onChange={(newValue) => updateSettings(newValue)}
-                                withCreateSuggestion={true}
-                            ></LinkControl>
-                        </MenuItem>
-                    </MenuGroup>
-                )}
-            </DropdownMenu>
-        </ToolbarGroup>
-    </BlockControls>;
-}
+    return (
+        <BlockControls>
+            <ToolbarGroup>
+                <DropdownMenu icon={customLink} label="Link">
+                    {() => (
+                        <MenuGroup>
+                            <MenuItem>
+                                <div style={{display: 'flex', flexDirection: 'column', gap: '12px'}}>
+                                    <LinkControl
+                                        searchInputPlaceholder="Search…"
+                                        allowDirectEntry={true}
+                                        forceIsEditingLink={true}
+                                        hasTextControl={true}
+                                        settings={[]}
+                                        value={{
+                                            url: link.url,
+                                        }}
+                                        onChange={(value) => {
+                                            update({
+                                                url: value?.url || "",
+                                                linkNewTab: !!value?.linkNewTab
+                                            });
+                                        }}
+                                        withCreateSuggestion={true}
+                                    />
 
-export default Link;
+
+                                    <PanelBody title="Advanced Settings" initialOpen={false}>
+                                        <Grid columns={2} columnGap={15} rowGap={20} style={{marginTop: '15px'}}>
+                                            <ToggleControl
+                                                label="Open in new tab"
+                                                checked={!!link.linkNewTab}
+                                                onChange={(v) => update({linkNewTab: v})}
+                                            />
+
+                                            <ToggleControl
+                                                label="Open in new window"
+                                                checked={!!link.newWindow}
+                                                onChange={(v) => update({newWindow: v})}
+                                            />
+
+                                            <TextControl
+                                                label="Title attribute"
+                                                value={link.title}
+                                                onChange={(v) => update({title: v})}
+                                                style={{gridColumn: '1/-1'}}
+                                            />
+
+                                            <TextControl
+                                                label="ARIA Label"
+                                                value={link.ariaLabel}
+                                                onChange={(v) => update({ariaLabel: v})}
+                                                style={{gridColumn: '1/-1'}}
+                                            />
+
+                                            <TextControl
+                                                label="Anchor ID"
+                                                value={link.id}
+                                                onChange={(v) => update({id: v})}
+                                            />
+
+                                            <TextControl
+                                                label="rel"
+                                                value={link.rel}
+                                                placeholder="nofollow ugc sponsored"
+                                                onChange={(v) => update({rel: v})}
+                                            />
+                                        </Grid>
+                                    </PanelBody>
+                                </div>
+
+
+                            </MenuItem>
+                        </MenuGroup>
+                    )}
+                </DropdownMenu>
+            </ToolbarGroup>
+        </BlockControls>
+    );
+}

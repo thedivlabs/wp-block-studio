ac7bd31e107b87269c7da8042787fd64ae6dc958
Alex Ferrao <aferrao@divlabs.dev>
2025-11-22 13:13:54 -0500

    Dev

---
+++ app/dev/js/components/LayoutFields.js
@@ -0,0 +1,45 @@
+import {memo, useCallback} from "@wordpress/element";
+import {
+    __experimentalToolsPanel as ToolsPanel,
+    __experimentalGrid as Grid,
+} from "@wordpress/components";
+import _ from "lodash";
+import {Field} from "Components/Field";
+
+export const LayoutFields = memo(
+    ({label = "Layout", settings = {}, suppress = [], updateFn}) => {
+        const {layoutFieldsMap: map = []} = window?.WPBS_StyleEditor ?? {};
+
+        const onUpdate = useCallback(
+            (slug, value) => {
+                updateFn({[slug]: value});
+            },
+            [updateFn]
+        );
+
+        return (
+            <ToolsPanel
+                label={label}
+                resetAll={() => updateFn({})}
+            >
+                <Grid columns={2} rowGap={15} columnGap={20}>
+                    {map
+                        .filter((f) => !suppress.includes(f.slug))
+                        .map((field) => (
+                            <Field
+                                key={field.slug}
+                                field={field}
+                                settings={settings}
+                                callback={(value) => onUpdate(field.slug, value)}
+                                isToolsPanel={false}
+                            />
+                        ))}
+                </Grid>
+            </ToolsPanel>
+        );
+    },
+    (prev, next) =>
+        _.isEqual(prev.settings, next.settings) &&
+        _.isEqual(prev.suppress, next.suppress) &&
+        prev.label === next.label
+);

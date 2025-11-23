e5b3835a6ff0c6b3df84dbac3a0e4547b3dc060a
thedivlabs <121818117+thedivlabs@users.noreply.github.com>
2025-11-23 14:54:57 -0500

    Dev

--- app/dev/js/components/HoverFields.js
+++ app/dev/js/components/HoverFields.js
@@ -9,6 +9,7 @@ export const HoverFields = ({
                             }) => {
     const {hoverFieldsMap: map = []} = window?.WPBS_StyleEditor ?? {};
 console.log(settings);
+
     return (
         <ToolsPanel
             label={label}
@@ -16,15 +17,25 @@ console.log(settings);
         >
             {map
                 .filter((f) => !suppress.includes(f.slug))
-                .map((field) => (
-                    <Field
+                .map((field) => {
+
+                    console.log(field);
+
+                    return (<Field
                         key={field.slug}
                         field={field}
                         settings={settings}
-                        callback={(value) => updateFn(value)}
+                        callback={(value) => {
+                            const nextHover = {
+                                ...settings,   // keep existing hover values
+                                ...value       // add the new patched values
+                            };
+
+                            updateFn(nextHover);
+                        }}
                         isToolsPanel={true}
-                    />
-                ))}
+                    />)
+                })}
         </ToolsPanel>
     );
 };

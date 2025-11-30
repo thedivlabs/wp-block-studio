7d00bfce065f8292aef142eb644ce53f81726a2a
thedivlabs <121818117+thedivlabs@users.noreply.github.com>
2025-11-30 15:48:27 -0500

    Dev

---
+++ app/dev/blocks/loop-card/block.js
@@ -0,0 +1,64 @@
+import {registerBlockType} from "@wordpress/blocks";
+import metadata from "./block.json";
+
+import {STYLE_ATTRIBUTES, withStyle, withStyleSave} from 'Components/Style';
+
+const selector = "wpbs-loop-card";
+
+const getClassNames = (attributes = {}, styleData) => {
+    const {"wpbs-loop-card": settings} = attributes;
+
+    return [
+        selector,
+        "w-full",
+        "block",
+        "relative",
+    ]
+        .filter(Boolean)
+        .join(' ');
+};
+
+registerBlockType(metadata.name, {
+    apiVersion: 3,
+    attributes: {
+        ...metadata.attributes,
+        ...STYLE_ATTRIBUTES,
+        "wpbs-loop-card": {
+            type: "object",
+            default: {},
+        },
+    },
+
+    edit: withStyle(
+        (props) => {
+
+            const {attributes, styleData, BlockWrapper, setCss, setPreload} = props;
+            const {'wpbs-style': settings = {}} = attributes;
+            const classNames = getClassNames(attributes, styleData);
+
+            return (
+                <>
+                    <BlockWrapper
+                        props={props}
+                        className={classNames}
+                        hasBackground={true}
+                        hasChildren={true}
+                    />
+                </>
+            );
+        }),
+
+    save: withStyleSave((props) => {
+        const {attributes, styleData, BlockWrapper} = props;
+        const classNames = getClassNames(attributes, styleData);
+
+        return (
+            <BlockWrapper
+                props={props}
+                className={classNames}
+                hasBackground={true}
+                hasChildren={true}
+            />
+        );
+    }),
+});

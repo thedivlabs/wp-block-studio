ce3b9bffc4a1b16e691e160236dfacd2e26a594e
Alex Ferrao <aferrao@divlabs.dev>
2025-12-02 15:03:21 -0500

    Dev

--- app/dev/blocks/loop-card/render.php
+++ app/dev/blocks/loop-card/render.php
@@ -6,17 +6,17 @@ $is_rest = function_exists( 'wp_is_rest_request' )
 	: ( defined( 'REST_REQUEST' ) && REST_REQUEST );
 
 
-if ( ! empty( $block->context['wpbs/termId'] ) ) {
-	$term_link = get_term_link( $block->context['wpbs/termId'] );
-	$content   = str_replace( '%%__TERM_LINK_URL__%%', $term_link, $content ?? '' );
-}
-
-if ( ! empty( $block->context['wpbs/termId'] ) ) {
-	$content = str_replace( '%%__POST_LINK_URL__%%', get_permalink(), $content ?? '' );
-}
-
 if ( ! empty( $attributes['isLoop'] ) ) {
-	
+
+	if ( ! empty( $block->context['wpbs/termId'] ) ) {
+		$term_link = get_term_link( $block->context['wpbs/termId'] );
+		$content   = str_replace( '%%__TERM_LINK_URL__%%', $term_link, $content ?? '' );
+	}
+
+	if ( ! empty( $block->context['wpbs/postId'] ) ) {
+		$content = str_replace( '%%__POST_LINK_URL__%%', get_permalink(), $content ?? '' );
+	}
+
 	if ( ! $is_rest ) {
 		echo '<script type="application/json" data-wpbs-loop-template>';
 		echo wp_json_encode(

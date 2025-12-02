dc91090a6094713c1ce9f1d40fe77a9ea6581e79
Alex Ferrao <aferrao@divlabs.dev>
2025-12-02 13:33:11 -0500

    Dev

--- app/dev/blocks/loop-card/render.php
+++ app/dev/blocks/loop-card/render.php
@@ -4,22 +4,20 @@ $is_rest = function_exists( 'wp_is_rest_request' )
 	? wp_is_rest_request()
 	: ( defined( 'REST_REQUEST' ) && REST_REQUEST );
 
+if ( ! empty( $attributes['isLoop'] ) ) {
 
-$content = str_replace( '%%__POST_LINK_URL__%%', get_permalink(), $content ?? '' );
+	$content = str_replace( '%%__POST_LINK_URL__%%', get_permalink(), $content ?? '' );
 
-if (
-	! empty( $attributes['isLoop'] ) &&
-	! $is_rest
-) {
+	if ( ! $is_rest ) {
+		echo '<script type="application/json" data-wpbs-loop-template>';
+		echo wp_json_encode(
+			$block->parsed_block ?? [],
+			JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE
+		);
+		echo '</script>';
 
-	echo '<script type="application/json" data-wpbs-loop-template>';
-	echo wp_json_encode(
-		$block->parsed_block ?? [],
-		JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE
-	);
-	echo '</script>';
-
-	return;
+		return;
+	}
 }
 
 echo $content ?? '';

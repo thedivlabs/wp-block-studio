553ccbb3e4a29d32140eea210428e27e76998598
Alex Ferrao <aferrao@divlabs.dev>
2025-12-02 18:32:56 -0500

    Dev

--- app/dev/blocks/loop-card/render.php
+++ app/dev/blocks/loop-card/render.php
@@ -5,28 +5,24 @@ $is_rest = function_exists( 'wp_is_rest_request' )
 	? wp_is_rest_request()
 	: ( defined( 'REST_REQUEST' ) && REST_REQUEST );
 
+if ( ! $is_rest && ! empty( $block->context['wpbs/isLoop'] ) ) {
+	echo '<script type="application/json" data-wpbs-loop-template>';
+	echo wp_json_encode(
+		$block->parsed_block ?? [],
+		JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE
+	);
+	echo '</script>';
 
-if ( ! empty( $block->context['wpbs/isLoop'] ) ) {
+	return;
+}
 
-	if ( ! empty( $block->context['wpbs/termId'] ) ) {
-		$term_link = get_term_link( $block->context['wpbs/termId'] );
-		$content   = str_replace( '%%__TERM_LINK_URL__%%', $term_link, $content ?? '' );
-	}
+if ( ! empty( $block->context['wpbs/termId'] ) ) {
+	$term_link = get_term_link( $block->context['wpbs/termId'] );
+	$content   = str_replace( '%%__TERM_LINK_URL__%%', $term_link, $content ?? '' );
+}
 
-	if ( ! empty( $block->context['wpbs/postId'] ) ) {
-		$content = str_replace( '%%__POST_LINK_URL__%%', get_permalink(), $content ?? '' );
-	}
-
-	if ( ! $is_rest ) {
-		echo '<script type="application/json" data-wpbs-loop-template>';
-		echo wp_json_encode(
-			$block->parsed_block ?? [],
-			JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE
-		);
-		echo '</script>';
-
-		return;
-	}
+if ( ! empty( $block->context['wpbs/postId'] ) ) {
+	$content = str_replace( '%%__POST_LINK_URL__%%', get_permalink(), $content ?? '' );
 }
 
 echo $content ?? '';

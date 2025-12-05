2654d9bde748b450be90c17e92028fb4e8ddd7a5
Alex Ferrao <aferrao@divlabs.dev>
2025-12-05 18:31:20 -0500

    Dev

--- app/dev/blocks/slider/block.js
+++ app/dev/blocks/slider/block.js
@@ -106,9 +106,8 @@ registerBlockType(metadata.name, {
         );
 
         useEffect(() => {
-            // Only update if the attribute value differs from the current isLoop
             if (attributes?.isLoop !== isLoop) {
-                setAttributes({ isLoop:!!isLoop });
+                setAttributes({isLoop: !!isLoop});
             }
         }, [isLoop, setAttributes]);
 

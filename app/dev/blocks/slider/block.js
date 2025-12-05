ea92c6764ddb7e36204a1fcc46aa1c85694c2b27
Alex Ferrao <aferrao@divlabs.dev>
2025-12-05 11:47:04 -0500

    Dev

--- app/dev/blocks/slider/block.js
+++ app/dev/blocks/slider/block.js
@@ -25,7 +25,7 @@ function getCssProps(settings) {
     const breakpoints = settings?.breakpoints || {};
 
     const slides = baseProps.slidesPerView ?? null;
-    const space = baseProps.spaceBetween ?? null;
+    const space = `${baseProps.spaceBetween ?? 0}px`;
 
     const css = {
         props: {
@@ -39,7 +39,7 @@ function getCssProps(settings) {
         const bpProps = bpEntry?.props || {};
 
         const slides = bpProps.slidesPerView ?? null;
-        const space = bpProps.spaceBetween ?? null;
+        const space = `${bpProps.spaceBetween ?? 0}px`;
 
         css.breakpoints[bpKey] = {
             props: {

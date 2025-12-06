a63a559048b7c6fa12dd47addfdeae9cb0a39473
Alex Ferrao <aferrao@divlabs.dev>
2025-12-06 11:41:44 -0500

    Dev

--- app/dev/js/modules/slider.js
+++ app/dev/js/modules/slider.js
@@ -4,17 +4,46 @@ import merge from 'lodash/merge';
 export default class Slider {
 
     static init() {
-        /* [...document.querySelectorAll('.wpbs-slider.swiper')].forEach(el => {
-            this.observe(el);
-        }); */
+        // You can uncomment this if you want auto-init on DOMContentLoaded
+        // [...document.querySelectorAll('.wpbs-slider.swiper')].forEach(el => {
+        //     this.observe(el);
+        // });
+    }
+
+    /**
+     * Deep merge args, merging objects into defaults
+     */
+    static mergeArgs(element, args = {}) {
+        // Clone default to avoid mutation
+        const merged = merge({}, SWIPER_ARGS_VIEW);
+
+        // Navigation always points to correct DOM elements
+        merged.navigation = {
+            ...merged.navigation,
+            nextEl: element.querySelector('.wpbs-slider-button--next'),
+            prevEl: element.querySelector('.wpbs-slider-button--prev'),
+            ...args.navigation,
+        };
+
+        // Pagination: merge only if object, else keep default
+        if (args.pagination && typeof args.pagination === 'object') {
+            merged.pagination = merge({}, merged.pagination, args.pagination);
+        }
+
+        // Merge any other top-level args
+        Object.keys(args).forEach(key => {
+            if (key !== 'navigation' && key !== 'pagination') {
+                merged[key] = args[key];
+            }
+        });
+
+        return merged;
     }
 
     static observe(element, args = {}) {
         if (element.classList.contains('swiper-initialized')) return;
 
         const mergedArgs = this.mergeArgs(element, args);
-
-        console.log(mergedArgs);
 
         const observer = new IntersectionObserver((entries, observerInstance) => {
             entries.forEach(entry => {
@@ -42,38 +71,12 @@ export default class Slider {
         observer.observe(element);
     }
 
-    static mergeArgs(element, args = {}) {
-        // Clone default to avoid mutation
-        const merged = merge({}, SWIPER_ARGS_VIEW);
-
-        // Navigation always points to correct DOM elements
-        merged.navigation = {
-            ...merged.navigation,
-            nextEl: element.querySelector('.wpbs-slider-button--next'),
-            prevEl: element.querySelector('.wpbs-slider-button--prev'),
-            ...args.navigation, // allow overrides
-        };
-
-        // Pagination: merge only if object, else keep default
-        if (args.pagination && typeof args.pagination === 'object') {
-            merged.pagination = merge({}, merged.pagination, args.pagination);
-        }
-
-        // Merge any other top-level args
-        Object.keys(args).forEach(key => {
-            if (key !== 'navigation' && key !== 'pagination') {
-                merged[key] = args[key];
-            }
-        });
-
-        return merged;
-    }
-
     static initLib() {
         if (!this._libPromise) {
             if (typeof window.Swiper === 'function') {
                 this._libPromise = Promise.resolve();
             } else {
+                // Load CSS
                 const stylesheet = document.createElement('link');
                 stylesheet.id = 'wpbs-swiper-styles';
                 stylesheet.rel = 'stylesheet';
@@ -81,6 +84,7 @@ export default class Slider {
                 stylesheet.href = 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css';
                 document.head.appendChild(stylesheet);
 
+                // Load JS
                 this._libPromise = new Promise((resolve, reject) => {
                     const script = document.createElement('script');
                     script.id = 'wpbs-swiper-js';
@@ -95,5 +99,4 @@ export default class Slider {
         }
         return this._libPromise;
     }
-
 }

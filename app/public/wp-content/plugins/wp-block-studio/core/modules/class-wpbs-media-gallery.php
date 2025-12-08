7fc6467732885ec38346c48d570c13691618064e
thedivlabs <121818117+thedivlabs@users.noreply.github.com>
2025-12-08 00:09:16 -0500

    Dev

--- app/public/wp-content/plugins/wp-block-studio/core/modules/class-wpbs-media-gallery.php
+++ app/public/wp-content/plugins/wp-block-studio/core/modules/class-wpbs-media-gallery.php
@@ -17,8 +17,6 @@ class WPBS_Media_Gallery {
 
 	private function __construct() {
 
-		add_action( 'acf/save_post', [ $this, 'clear_transients' ], 20 );
-		add_action( 'rest_api_init', [ $this, 'register_endpoint' ] );
 
 		$args = [
 			'supports'      => [ 'title', 'editor', 'permalink', 'thumbnail', 'excerpt' ],
@@ -39,52 +37,6 @@ class WPBS_Media_Gallery {
 
 		WPBS_Taxonomy::register( self::TAX_SINGULAR, self::TAX_PLURAL, self::SLUG, self::TAX_SLUG, false );
 
-	}
-
-	public function render_block( WP_REST_Request $request ): WP_REST_Response {
-		$unique_id     = $request->get_param( 'uniqueId' );
-		$block_context = $request->get_param( 'blockContext' );
-
-		$response_data = array(
-			'success'  => true,
-			'rendered' => ( new WP_Block( [
-				'blockName' => 'wpbs/media-gallery-container',
-				'attrs'     => [
-					'unique_id'    => $unique_id,
-					'blockContext' => $block_context,
-				]
-			] ) )->render(),
-		);
-
-		// Return a WP_REST_Response object
-		return new WP_REST_Response( $response_data, 200 );
-	}
-
-	public function register_endpoint(): void {
-		register_rest_route( 'wpbs/v1', '/media-gallery', array(
-			'methods'             => 'POST', // Or 'GET' if you prefer, but POST is fine for sending data
-			'callback'            => [ $this, 'render_block' ],
-			'permission_callback' => '__return_true', // Allows public access (no authentication required)
-			'args'                => array(
-				'uniqueId'     => array(
-					'type'              => 'string',
-					'sanitize_callback' => 'sanitize_text_field',
-					'validate_callback' => function ( $param, $request, $key ) {
-						// Basic validation: check if it's not empty
-						return ! empty( $param );
-					},
-				),
-				'blockContext' => array(
-					'type' => 'object',
-				)
-			),
-		) );
-	}
-
-	public function clear_transients( $post_id ): void {
-		if ( get_post_type( $post_id ) === self::SLUG ) {
-			delete_transient( self::TRANSIENT_PREFIX . $post_id );
-		}
 	}
 
 	public static function init(): WPBS_Media_Gallery {

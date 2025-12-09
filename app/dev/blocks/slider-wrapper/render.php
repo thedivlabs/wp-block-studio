e07b413eddc6d1ce1880398d8e3b27e675e7ab4a
Alex Ferrao <aferrao@divlabs.dev>
2025-12-09 15:51:25 -0500

    Dev

--- app/dev/blocks/slider-wrapper/render.php
+++ app/dev/blocks/slider-wrapper/render.php
@@ -30,16 +30,14 @@ $loop_data = $loop_instance->render_from_php(
 	max( 1, get_query_var( 'paged', 1 ) )
 );
 
-WPBS::console_log( $loop_data );
-WPBS::console_log( $merged_query );
-
 
 // Build wrapper attributes using the core block wrapper helper.
-$wrapper_attrs = get_block_wrapper_attributes( [
-	'class' => trim( implode( ' ', array_filter( [
+$wrapper_attrs = get_block_wrapper_attributes( array_filter( [
+	'class'         => trim( implode( ' ', array_filter( [
 		'swiper-wrapper',
 	] ) ) ),
-] );
+	'data-lightbox' => $is_gallery ? json_encode( $loop_data['lightbox'] ?? [] ) : null,
+] ) );
 
 // Open wrapper
 echo '<div ' . $wrapper_attrs . '>';

36fa97905f533266f40f274614a7a082f2b3bc04
Alex Ferrao <aferrao@divlabs.dev>
2025-12-11 08:09:07 -0500

    Dev

--- app/dev/blocks/slider-wrapper/block.js
+++ app/dev/blocks/slider-wrapper/block.js
@@ -35,7 +35,7 @@ registerBlockType(metadata.name, {
             const {attributes, styleData, BlockWrapper, setCss, setPreload, setAttributes, context} = props;
             const classNames = getClassNames(attributes, styleData);
 
-            const {isGallery, isLoop} = attributes;
+            const {isGallery, isLoop} = context;
 
             useEffect(() => {
                 const next = {

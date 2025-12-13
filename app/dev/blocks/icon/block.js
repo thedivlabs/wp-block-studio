4afa60636831d7705f8a60669c17eadaa2f21d7b
Alex Ferrao <aferrao@divlabs.dev>
2025-12-13 08:35:08 -0500

    Dev

---
+++ app/dev/blocks/icon/block.js
@@ -0,0 +1,205 @@
+import {registerBlockType} from "@wordpress/blocks";
+import metadata from "./block.json";
+
+import {InspectorControls} from "@wordpress/block-editor";
+import {__experimentalGrid as Grid, PanelBody} from "@wordpress/components";
+import {useCallback, useMemo} from "@wordpress/element";
+import {isEqual} from "lodash";
+
+import {STYLE_ATTRIBUTES, withStyle, withStyleSave} from "Components/Style";
+import {Field} from "Components/Field";
+import {IconControl, MaterialIcon} from "Components/IconControl";
+import Link, {getAnchorProps} from "Components/Link";
+
+const selector = "wpbs-icon-block";
+
+/* --------------------------------------------------------------
+ * CLASSNAMES
+ * -------------------------------------------------------------- */
+const getClassNames = (attributes = {}) => {
+    const {"wpbs-icon-block": settings = {}} = attributes;
+    const isInline = settings?.inline ?? false;
+
+    return [
+        selector,
+        isInline && "--inline",
+    ]
+        .filter(Boolean)
+        .join(" ");
+};
+
+/* --------------------------------------------------------------
+ * BLOCK REGISTRATION
+ * -------------------------------------------------------------- */
+registerBlockType(metadata.name, {
+    apiVersion: 3,
+
+    attributes: {
+        ...metadata.attributes,
+        ...STYLE_ATTRIBUTES,
+
+        "wpbs-icon-block": {
+            type: "object",
+            default: {},
+        },
+    },
+
+    edit: withStyle(
+        (props) => {
+            const {attributes, BlockWrapper, setAttributes} = props;
+            const {"wpbs-icon-block": settings = {}} = attributes;
+
+            const classNames = getClassNames(attributes);
+
+            const isImage = Boolean(
+                attributes?.className?.split(" ").includes("is-style-image")
+            );
+
+            const isLink = Boolean(settings?.link?.url);
+            const anchorProps = isLink ? getAnchorProps(settings.link) : {};
+
+            const updateSettings = useCallback(
+                (nextValue) => {
+                    const next = {
+                        ...settings,
+                        ...nextValue,
+                    };
+
+                    if (!isEqual(settings, next)) {
+                        setAttributes({
+                            "wpbs-icon-block": next,
+                        });
+                    }
+                },
+                [settings, setAttributes]
+            );
+
+            const BlockContent = useMemo(() => (
+                <>
+                    {isImage && settings?.image?.url && (
+                        <img
+                            src={settings.image.url}
+                            alt={settings?.image?.alt || ""}
+                            aria-hidden={true}
+                        />
+                    )}
+                    {!isImage && <MaterialIcon {...settings?.icon} />}
+                </>
+            ), [isImage, settings?.image, settings?.icon]);
+
+            return (
+                <>
+                    <InspectorControls group="styles">
+                        <Link
+                            defaultValue={settings?.link}
+                            callback={(val) => updateSettings({link: val})}
+                        />
+
+                        <PanelBody
+                            initialOpen
+                            className="wpbs-block-controls"
+                        >
+                            <Grid columns={1} columnGap={15} rowGap={20}>
+                                {!isImage && (
+                                    <IconControl
+                                        fieldKey="icon"
+                                        label="Icon"
+                                        props={props}
+                                        value={settings?.icon}
+                                        onChange={(val) =>
+                                            updateSettings({icon: val})
+                                        }
+                                    />
+                                )}
+
+                                {isImage && (
+                                    <Field
+                                        field={{
+                                            type: "image",
+                                            slug: "image",
+                                            label: "Image",
+                                            full: true,
+                                        }}
+                                        fieldKey="image"
+                                        props={props}
+                                        value={settings?.image}
+                                        callback={(val) =>
+                                            updateSettings({image: val})
+                                        }
+                                        isToolsPanel={false}
+                                    />
+                                )}
+
+                                <Grid columns={2} columnGap={15} rowGap={20}>
+                                    <Field
+                                        field={{
+                                            type: "toggle",
+                                            slug: "inline",
+                                            label: "Inline",
+                                        }}
+                                        fieldKey="inline"
+                                        props={props}
+                                        value={settings}
+                                        callback={(val) =>
+                                            updateSettings({inline: !!val})
+                                        }
+                                        isToolsPanel={false}
+                                    />
+                                </Grid>
+                            </Grid>
+                        </PanelBody>
+                    </InspectorControls>
+
+                    <BlockWrapper props={props} className={classNames}>
+                        {isLink ? (
+                            <a {...anchorProps}>{BlockContent}</a>
+                        ) : (
+                            BlockContent
+                        )}
+                    </BlockWrapper>
+                </>
+            );
+        },
+        {hasChildren: false, hasBackground: false}
+    ),
+
+    save: withStyleSave(
+        (props) => {
+            const {attributes, BlockWrapper} = props;
+            const {"wpbs-icon-block": settings = {}} = attributes;
+
+            const classNames = getClassNames(attributes);
+
+            const isImage = Boolean(
+                attributes?.className?.split(" ").includes("is-style-image")
+            );
+
+            const isLink = Boolean(settings?.link?.url);
+            const anchorProps = isLink ? getAnchorProps(settings.link) : {};
+
+            const BlockContent = (
+                <>
+                    {isImage && settings?.image?.url && (
+                        <img
+                            src={settings.image.url}
+                            alt={settings?.image?.alt || ""}
+                            aria-hidden={true}
+                        />
+                    )}
+                    {!isImage && <MaterialIcon {...settings?.icon} />}
+                </>
+            );
+
+            return (
+                <BlockWrapper props={props} className={classNames}>
+                    {isLink ? (
+                        <a {...anchorProps}>{BlockContent}</a>
+                    ) : (
+                        BlockContent
+                    )}
+                </BlockWrapper>
+            );
+        },
+        {hasChildren: false, hasBackground: false}
+    ),
+});
